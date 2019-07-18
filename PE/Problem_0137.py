813447bd6e6f052f920f6ba4c24df103ab00dc41
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-07-18 22:48:21 +0800

    Create Problem_0137.py

---
+++ PE/Problem_0137.py
@@ -0,0 +1,27 @@
+# Consider the infinite polynomial series
+# A_{F}(x) = x F_{1} + x^{2} F_{2} + x^{3} F_{3} + ...,
+# where F_{k} is the kth term in the Fibonacci sequence
+# 1, 1, 2, 3, 5, 8, ...;
+# For this problem we shall be interested in values of
+# x for which A_{F}(x) is a positive integer.
+# We shall call A_{F}(x) a golden nugget if x is rational,
+# because they become increasingly rarer.
+# Find the 15th golden nugget.
+# ----------------------------------------------------
+# Analysis: Pell related equation
+#           A_{F}(x) = x / (1 - x - x^{2})
+#           Hint: http://oeis.org/A081018
+#                 https://www.alpertron.com.ar/QUAD.HTM
+#                 https://en.wikipedia.org/wiki/Pell%27s_equation
+
+20df25a94c97a396426fa855c71da9366be04ff288faef0931ac9756fe71a03242eabe9ed297d0dda53163dfde6ad82c76fe
+ea1da34004eab60c8f867fa8f96562f6269331ad2fee02b40f8cbebe971871b806ee9bbadc0b9b5f6fbf72d4b689221cbf59
+578a476fd6f5a9d73c33c42f45b21a153283402463fb753f3ced1508ec19c977d1297640088e4a3bc9776c9e14e05bb3c317
+23bfd7b55cb5c2d720c0e635ee222e49fffdfcb65b6c6c5abe85ba32d6f92a5376b9b29ef113b2ae143b525b9741c70cd24b
+7aadbb4fd7ab9fc6e60fbf356bfcb31b4ca5217a0d959f8009c135126410f57ba0df0c8155981fbb9f0473b4bfbfe509efce
+17c94bede5f77c2d2d7ca1bece0d396cc5c81a1a994ccafeb2daf65ab9bbb885571420b9649da023c06f0304944e2f8b4dd4
+afb71a90aeefbef66b20313122c63ab4abf5db85712131d08f8d1d05929546f681e6ea0d6246260ba90b4c98318d22b48087
+67a9d3a5883a126fd7e47f4e824f459c8ac0e35a33780f94983cfbfeb7c0527585bfaa522007ad9d1dbc45b7dc06a897c0d3
+68096577522db3e4fb215fea452174d69869a65ed083bee03ad8ef955ce11b7d24255fd71d6c4aba6cfd6a0e9c26c23da9dd
+36777578684cf6eff49e0e81f09bdbe5a1b6cd5e8dea5227885a3c72dd35b0e313040d0c9e87c5a086455b9f1f49d6678c78
+1bfd92350560477a53f0d0da4b90b430
