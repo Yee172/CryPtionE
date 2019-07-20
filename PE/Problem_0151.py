f3e334103018b448d40fdfdf5267dc10ffdb0bdf
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-07-20 09:44:40 +0800

    Create Problem_0151.py

---
+++ PE/Problem_0151.py
@@ -0,0 +1,36 @@
+# A printing shop runs 16 batches (jobs) every week and
+# each batch requires a sheet of special colour-proofing
+# paper of size A5.
+# Every Monday morning, the foreman opens a new envelope,
+# containing a large sheet of the special paper with
+# size A1.
+# He proceeds to cut it in half, thus getting two sheets
+# of size A2. Then he cuts one of them in half to get
+# two sheets of size A3 and so on until he obtains the
+# A5-size sheet needed for the first batch of the week.
+# All the unused sheets are placed back in the envelope.
+# At the beginning of each subsequent batch, he takes
+# from the envelope one sheet of paper at random. If it
+# is of size A5, he uses it. If it is larger, he repeats
+# the 'cut-in-half' procedure until he has what he needs
+# and any remaining sheets are always placed back in
+# the envelope.
+# Excluding the first and last batch of the week, find
+# the expected number of times (during each week) that
+# the foreman finds a single sheet of paper in the
+# envelope.
+# Give your answer rounded to six decimal places.
+# ----------------------------------------------------
+# Analysis: brute force
+
+33a873559591273051944858c694679be23ff54a6e087042460bc4982639fd6afc3bbeffce8fc284c6a1bd487d0c8eaae2de
+b91d7c4cede053f2128b27c52f95322de265b5373c1a2d1a1342772a7c1addc03447684dadc8941649fbe866365f30cbf390
+3280bdc6e0f027704451952cadcb86adb664a4fcaab957c575e6451fb04dea38a1f062351fea7394d0dce50128f367692f86
+98c5683e0cbc6695778bd7b3592e943020d27c762d008c622735a729da3b418059349908a201b5290a8ce3aa24c29c72a0eb
+6f36aeefd63a63328431da685a883f1f7381f30a1b344b258b9ef6056490fb90d3802eb08a97df24dfdbee483650827f501a
+7ec121011b3d7c2d2d7c09c6edf0d5eb9e71ea9937c5a6e7cbdbec5194fa2b1a8b252da63d826410bc0b9fe9021f7ecf0ce6
+07031efea5243df667746a6d8f0790cf8571416a8251847ccc0fa684494434eadcfef0e4013d93892df97828875cb93b5e9e
+64c959bbaff2c7095f710afa10bb0f08c69d25f9c7600364c10d4302d9a71db7e337ef808d852e3a1de5911d282fc2727437
+e1dde2ad519040e7e6988113358ea0026451ec93762ef363880456eb990ee8386fb0833dca5d57b5783a54e7a946c5d7f3a7
+6fab81949ec34435852a1333c72d6815f889ed81274dc191a91fcfb69c478d3cdb43b641e17cd5eda433651997718c1a5099
+730e4fd8f2fea96dff0cb986bdb25572
