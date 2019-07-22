c9c4b2cb6802d2523f99ed414fe6428212dfb948
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-07-22 14:18:41 +0800

    Create Problem_0135.py

---
+++ PE/Problem_0135.py
@@ -0,0 +1,23 @@
+# Given the positive integers, x, y, and z, are consecutive
+# terms of an arithmetic progression, the least value
+# of the positive integer, n, for which the equation,
+# x^{2} − y^{2} − z^{2} = n, has exactly two solutions
+# is n = 27.
+# It turns out that n = 1155 is the least value which
+# has exactly ten solutions.
+# How many values of n less than one million have exactly
+# ten distinct solutions?
+# ----------------------------------------------------
+# Analysis: brute force
+
+8d0e8bbebce39a952dea5a258bc8a9bf157cf2826ed3ffba35920928110208704943986ae80eccf50c2ad99611fb22004c71
+7950284e67764315c5d2106b29cf7c619f9250a00d5da60bc299e88fc078e306123f4cff251ac08c913d2985ace65aa79ec9
+1d509b9e9374a5e8632c69c4d051b0ee0db0e80ede84c336694a7807cca05ad813afa0db0edc5a9f746d3a938466cc856802
+c9e9bf417a1bf10d0d2b5c59b84aba0593a8b0ca8ae843753c1fa337cf92c4fb65a2bcb1e92a4d0dc4e7b0a332ac465787e1
+0b7d4d0ac17a85c2be1f828d94469007786d9c53e8156cde3b0528a4b6b018601ac8ba30f2750e1e99d47c75123b949fe726
+cb5479fe16a57c2d2d7c9743f04f774a33c4ee3bf6f275bacfab489905d718f317beb10458ef149c2c2bfdcc9c61d955040f
+5f2da9570f5d95db247447d3e4bb866bd96c67286819a7810942b162fdee13865afb4371a906382f3994b7a773fbf8c0f017
+cc52326000436309da5ecb3368dbf2d577c127937b8e892fa1a479b88314605b0554ff785d7d313efc08d0c34ef180db4b29
+026585b4ce57f296d95edc6cdaf2f9897037ef588001fca014bfab58b1693aee862d05298c9f8d2ef99e476e324a6a635006
+b1211cbd7deb285631c79a27277eeff932d0940a0959441be87957353680697743a81420cf1b274458ce5cf78c42006b22c0
+fc5f23854408c6096f2ef61f77f2d771
