885ce405d1ac9b05452e76c4ec4de7f6376e0887
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-07-23 08:24:50 +0800

    Create Problem_0136.py

---
+++ PE/Problem_0136.py
@@ -0,0 +1,22 @@
+# The positive integers, x, y, and z, are consecutive terms
+# of an arithmetic progression. Given that n is a positive
+# integer, the equation, x^{2} − y^{2} − z^{2} = n, has
+# exactly one solution when n = 20.
+# How many values of n less than fifty million have exactly
+# one solution?
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: Only the number at the form of n = p (p % 4 = 3),
+#                 n = 4 p, n = 16 p, where p is odd prime or 1, have only one unique solution
+
+668c063179c37e4c231e75f20ed161d785187ad22e7d2fa726d50947582b31e641a4a7f5334e1295642ce3c4493fe2e23763
+6bd20074cd42e40a71ec8ff5c3235c51310f6b64610b3ed60851f6ccc8d10b3b6674f777fd6d11e3e9e6cd94d7056180f1c5
+6af61428a5f1b61c59184405dc07ad6ea37aa47c15b8c840564aa529843da86f0dd1dd4a5977970f88524050800299c56e4c
+095cada46c909bf5380c343fb353f5595454f21a9ae35d6acaf4bffe9db72b73b68d4790d7d8df90f5d0d2f3d389810494d4
+6ffaa31ba52149aa7d84f4ecbfdb1be371dc3e468e3b89b4e36dc941b42869d40d3fc4926775654adf7594ec1ab582f39966
+4c06a5d251777c2d2d7cc46b1d9c3b6960eaadb8c0863a410def100b42b944cf18dcb0ca0f4f2396dd004f5ff25c42f85c27
+ad5aae63fe7863b8ce76b0f2d8173cf60c61340bb222327bb3afadf74d9b696a1761b77fcb750b4223ad8cec837d0bcc0d0f
+28eec7edd27313919924437b28ce8735e452903afe5d83405232b1491aab161b5a79e2b97582dad560fd58088b0daf942144
+9e0ebbc29b7846aca506d2f5677a39c83753fd89788bfeef18d23ae3322c61cdc2fe71322172070bb84542b65eb42af027e0
+d0f854b3c5841e9b9cf68c4e4efcb71fc9670f0bf1a618a8d046fdee90293ccae0d4ba9d115361d2a3777b61ba3c6603f7aa
+b0ee17944b78296cf184dd0244b7ea1b
