9641805da7b73edccfa05f3dd9685192b874b95f
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-07-28 14:42:02 +0800

    Create Problem_0207.py

---
+++ PE/Problem_0207.py
@@ -0,0 +1,19 @@
+# For some positive integers k, there exists an integer
+# partition of the form 4^{t} = 2^{t} + k, where 4^{t},
+# 2^{t}, and k are all positive integers and t is a real
+# number.
+# Partitions where t is also an integer are called perfect.
+# For any m \geq 1 let P(m) be the proportion of such
+# partitions that are perfect with k \leq m.
+# Find the smallest m for which P(m) < 1 / 12345.
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: consider the equation x^{2} = x + k,
+#                 where x = 2^{t} must be an integer
+
+375b1f790146e8a2d64db9f505f8a9ff99ccd07376a0d51521f7d538d0fad7518601c701dc81e0b90d51524a556d58585eff
+5337a8209f61924ae367647f75e80c646a1edb4bdd528eba1bcc2e7629724d5aa26dd2387955959cb22ab1d4b9d70bf261ca
+76a2cfa9779f324446cfc2783f398cacc8e0c20cfa72f41bb8e673f40574565546efd9b1cc7ec29833b01fc7a52344d272fa
+437014192aefdfa56d4da81eb680550a5ae2f8d178fd613daa2d44d40ae6e461334fdac82db58dd33a56a6185527cc3cd739
+b68f7a4f8059a4bbc7b461245e29fde126f9c9daffaa5a69bd8ba965113a3e1eb7d6e732f9a78770f4a3bccec2bd74811add
+3439f973619b
