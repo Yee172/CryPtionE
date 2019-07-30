572bbfb83c4db87107bf41bded4bd78cfd827940
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-07-31 07:25:29 +0800

    Create Problem_0317.py

---
+++ PE/Problem_0317.py
@@ -0,0 +1,24 @@
+# A firecracker explodes at a height of 100 m above level
+# ground. It breaks into a large number of very small
+# fragments, which move in every direction; all of them
+# have the same initial velocity of 20 m/s.
+# We assume that the fragments move without air resistance,
+# in a uniform gravitational field with g = 9.81 m / s^{2}.
+# Find the volume (in m^{3}) of the region through which
+# the fragments move before reaching the ground.
+# Give your answer rounded to four decimal places.
+# ----------------------------------------------------
+# Analysis: envelope of a family of parabola
+#           https://en.wikipedia.org/wiki/Envelope_(mathematics)
+
+724a8bd4e8ac155a9b70da39018ceba24fcbfdfce7a03523b9e466d2a0485357ad0870315b6f37a0fe3f095da4a2b378d3c4
+e185d5ab52f6f705c3af1f14da3b6d9c8846ab71cd323216e41b8a0424f7c203109a6cb1017828a7eee6cb7ba5a739d4a423
+1fc3beddee5123c02bcaae50478a333447ca7d2b6332f0bbd2961d111d3b6850665a13b3962021f30af344fd0f070a327e76
+181fadeaa84eb93d75bd5b81e8eb0686214fa3546bf5e94e2f71e6676ae4d8b4ee06d11ef77b5c4d3fcf89c8a78e487318e4
+5c47e0150ddaa3bf9bb83090514ca0682af397672bc8c42d95e17f77a536cc5a41b98247391c9222f554e579c01242fcab0c
+a657400c49617c2d2d7c369090a9779f233c4b88a0a77ee93970ee410d19c0955b6e3b07d336729fb25b25d55039c832df1f
+acd3b2b936d1fe5b46ffa1d216eb96c4274c5a57030f74122773f1908649cdb7758cb41bdd657527cc011a631a391a5e976a
+8004260553bf5ea98b0ca4fb4554b1c74fb3d753812785287316a56bd9002a5be3828c24c5debdd8aeb70adc7664507429b6
+3145b8b527832b40ad0414eda2b86b5e43349941210c1018c633d6c60dcf56cb3633053f3330ff64ee1523c04b3927c5e619
+3d2d170258531329e18d223ff1db34b929c261f9074af67e9dcf1516e3e7fc7126fd18157fc129cfb5d920bc62c86ae34721
+6e2761a29190674f49f61c43549d6ebf
