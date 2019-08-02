44a4a47de3e30da531b4580d40e5ced0861cd803
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-08-02 09:50:50 +0800

    Create Problem_0267.py

---
+++ PE/Problem_0267.py
@@ -0,0 +1,38 @@
+# You are given a unique investment opportunity.
+# Starting with £1 of capital, you can choose a fixed
+# proportion, f, of your capital to bet on a fair coin
+# toss repeatedly for 1000 tosses.
+# Your return is double your bet for heads and you lose
+# your bet for tails.
+# Choosing f to maximize your chances of having at least
+# £1000000000 after 1000 flips, what is the chance that
+# you become a billionaire?
+# All computations are assumed to be exact (no rounding),
+# but give your answer rounded to 12 digits behind the
+# decimal point in the form 0.abcdefghijkl.
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: ((1 + 2 f) + (1 - f))^{1000}
+#                 diff((1 + 2 f)^{n} (1 - f)^{1000 - n}) = 0 to get the optimum f
+
+464d18f974eca6446e3df088f749d23fa885845bb1307d9e5b27d4b274266dec95aa023826fd9012a151a46ac2aff68389a7
+efff4d5cfbe376ec73e3b991cde4026cb8aca06eb8d38593be55c87e7ccc6b7b1be8fab6860fa4fc35b502b9dee7926599bf
+8628378216070bbd76db069a8d32ec776a76b8bcc7b51cf959b4d4d6075f73df1fa04278fc50467cbab2c291a92b9290a25a
+681acb8a40edb0a9ecafda190297fcb189c13298473dc7db9e8d6cc781f8dcddb5983dbc42dc23b469fd173929462215dc8e
+a60f0d0fb23a0a5e206f30102bd94727edc91dcb201a605d25219f4e09efad50b17635c8cd19422c52665504836838dc486d
+706f1b09fd617c2d2d7c7c47ba9b66f5fc74bbcc2f1addbcef3ea4be401988b24a51244562a035c1d9486d55c1b4cf8b4836
+7e893107370f6e73273224974f23c363e47e2bc1f200da69b02727c475dd322013550ab7165b8415ac70fd1181539d4bb2eb
+71e8f1158e92a51a2780a5da0ce2fde5a2371972f7a3d013ce0637f50f1cf9d97f735f694f4308fb46b3a69f8f68145fab78
+dc9942d0d0543a059db3645e5b872a990c743db971d92b47a75fdd754be0d096624879ac7c04f55591784060c6f3a543ced5
+af479d525bcd3cae6f6b1664e2ed9deada5be954c766a270d227b38d1b2e07b6880d7dfafdfd274cca4b942f092fd5a0fe10
+1274d838b65a66d996715056ea68be577c2d2d7c2b8fd61ec0d2cb7cf7a3905cd094b5da5ea0dc422ee7dca4ade13ed6aba6
+a4424e33156e512045355910db9113b3d9aa13e3e7ac088a8d44a5aaf7d928c42a3e54708a0851c30aabbd2f418f489b74b1
+e85b4aa63f48bcd349ab059399f6b127531e0b712e4afdf169a6828a3dbc05ea570e8db90519b00604654485bb5fe466cf0a
+448a0a99f1c442fc292cfe1bc0cd1b8cdcf93867d91011d45bc61f4b5fff3681c18b24eb2cc48124e7f67165677eef048c0e
+0b0e8732e263cae725abcdf33e1a9ad93ec87959dd3b858a62012a657886bdd356fb7049501d5fe4a9937cbde280ae98ce73
+ff7f725f49e2ca6e6b8478f3f988d8f7ef59d5d211f4460929017c2d2d7c4c3d37bca325ef78cee04c98cf0cea66682fedaf
+ad5a700b6e465267419dc739edf8017506ffed332682f440e0ae489960ec088a33567eb490bf64233e5e19cc4590b8d7fba4
+37d1994146bc2bfea65c5c48970a9c6e96b3fccdbcd448525e55b41d76176d45dd73bbcec56de7116a23be8a6ae099fd51f4
+968a1ce6b54d3e9e11aa00371b3c853a55a79ec65e016cc3f9ceb8d3103eb4f19e3602fc93633d6c80a05cf59d4d541842b7
+f3be8226e73dc03ddf16b195db0e20981247b704611e7fb11eacfecd20d618b365f087e234d5de54da131bc6cb617a480a9d
+bb958cbfa91fe7d6f2afc9ce915c0ac813f1496173902a337032cfadecd453dc421dc3cc
