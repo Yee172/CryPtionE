04540b3f3d23aec1c4534f97ba9b1463a6913023
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-08-06 13:03:25 +0800

    Create Problem_0144.py

---
+++ PE/Problem_0144.py
@@ -0,0 +1,43 @@
+# In laser physics, a "white cell" is a mirror system
+# that acts as a delay line for the laser beam. The beam
+# enters the cell, bounces around on the mirrors, and
+# eventually works its way back out.
+# The specific white cell we will be considering is an
+# ellipse with the equation 4 x^{2} + y^{2} = 100.
+# The section corresponding to −0.01 \leq x \leq +0.01
+# at the top is missing, allowing the light to enter
+# and exit through the hole.
+# The light beam in this problem starts at the point
+# (0.0, 10.1) just outside the white cell, and the beam
+# first impacts the mirror at (1.4, -9.6).
+# Each time the laser beam hits the surface of the ellipse,
+# it follows the usual law of reflection "angle of incidence
+# equals angle of reflection." That is, both the incident
+# and reflected beams make the same angle with the normal
+# line at the point of incidence.
+# How many times does the beam hit the internal surface
+# of the white cell before exiting?
+# ----------------------------------------------------
+# Analysis: brute force
+
+07161db79a110a3f3abc3c14254a2cb5814cedf4493158baca3f15e865849577c013e5f10665fc40c03d5e881fb6c5f7642e
+d19f2b416ac4b91ffc9df0d48523647d750245ce6b1eacfabfb53c65dcd2bfb9d6b17047cf08a5953a58edaf3954e4c761f7
+02a8cf969721d10bc5dd69c8d3a8d304f51a178f238ac02dd6b0586bea37b1527c9da2a2a9b16bf3c216383b261a5a51eeb0
+d8a1b4acc070b56b2002f5058e3f2a1eafd630d97f6fbbe83fd3944c2032217cdcb16e6526d94f2cfcbcb636cf207e024ba5
+2c7f111b1f69e45878a8f1a09ea8911d22e1b532c923c0e4e83d88ef886e4eddce7427a53079aee21b4c52eb92c0ac5a6fd4
+92ede05424927c2d2d7c201cdee7813f1ec9946bcdd7f90d2b02f2e95d957466762d07e578f8211b68e95bd2b479319203d4
+2fb3fb8964a0d14ad950e2f677949d34b31b3e944a8d2b81ab50a748bdf650e3077aa439c7a36362f2ea6433cc87c255c2f7
+85a415976027d1e5b662df1108d25e0b756a8aa35e5165dc9928a69e2ad19eb77fc82eca05b8bde93b82a337318214b05bde
+df70b694bc4cc3516e4ba94b8168433ed1f3700181c49671d14cf45d7c520415948af182956aecacbd299abd23eff1db1607
+a9ec91473d247606742240bd27282ed813aed840d94cdaf4ea29c2233be1c827ec68dffa598951d9f59bcc5eef5c81c1517b
+dd8e7cea23f237e76f494283eb0924f87c2d2d7c840d9d0325e4b9c522a24708a3d4d76b4f426cdfe7422921b8609903f2b6
+08575d021aef2c5261eab143fae95cf3488189fc40d2a54b3d896c037853617df039802c6396ea7b1da10f84a013ef7651fa
+c65186906d4a16c2134bbdc717bf4ca9679844bd5e2dabb0157c6f251c104be807e91837d629cf62b2fe600ea4161afb9d92
+30989c8e90d5488aff398addf7e540e62d0ebc1346bdccc7505f30548efa0be834dd271d35392efe881a64c717491378c2aa
+27dba104866b8f74fa1c8005bb91ef6f61c5b9b2511afcc69641708556db24df540b9883025209a48705dd5b059eaa41ca4f
+e44e03fe50a385bc7d292409ec55e041271d9b2df20ef4b00db87c2d2d7cc8a96582df305856d9815d48b557a7db8cdf6c1b
+98e763cb1e2f99663c1e3737736df81d8331e2062071fc4ac09f968b8c31f1a68ab4bae4324c5628ab4759abb1be8230bb58
+88bf787b790065354e55493ddc7f0e4699de7452b4e1ec7b1f941bbdb1542a31a46cd7ce20805fd861fb8804182707ec698b
+ceea90224b6b270f4fc5136885c58108bafaef9b1527f050045de926c8beedf97f7709ad34fe9a26e51d03036ceab39768dc
+6db0398c746b1c4d1baf13a376a21fc59e5e885471ff45582d358a0b9e62799ffccf0c91e64c8dbe168deb4e6d84d881aff4
+eceaf1e29b24979eb5681fe85373fc66201c3dce9160f63dacb4dc3f7bc15655d7a69f3a
