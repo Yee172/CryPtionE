e6567fb38638c7d63bdac84ffb76c627ef426753
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-08-15 10:54:17 +0800

    Create Problem_0190.py

---
+++ PE/Problem_0190.py
@@ -0,0 +1,22 @@
+# Let S_{m} = (x_{1}, x_{2}, ..., x_{m}) be the m-tuple of
+# positive real numbers with x_{1} + x_{2} + ... + x_{m} = m
+# for which P_{m} = x_{1} * x_{2}^{2} * ... * x_{m}^{m}
+# is maximised.
+# For example, it can be verified that [P_{10}] = 4112
+# ([ ] is the integer part function).
+# Find \sum [P_{m}] for 2 \leq m \leq 15.
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: partial derivative
+
+5404dbef2a1c4de2f14ed144db0af28d110991bf08af9a9c9b0a01859e710d82f4d109ed90b65661ff8805fe1e5ae178db70
+65d4e815e5bc05a8ce3dc5b15447fc0ff9383316f18e1852dabbf0d47e26cb467e21ecc015fdf67a3b7c9ff0ee11e65e60f9
+4626ccff0c49bc3cb8b8b8cfdb3c90153c7b6f9f5f6d920e55a1f6a61789a624b1789b689d0cf8c8f3f4fd96df6f2f853164
+20aa4e7245f2f01060d5954542415dad38d813d9005b0b2bbc434aaf16b98b0b8ca6a0619950c5a226aa399048b7cf3cea69
+84bb693f4b1de4ee7c8036c834d94f8c0c12bc3fa431f40a8681031b426cf4e7ad4f9cab7c58097bccddc58bc49880c29dba
+43c344d41c187c2d2d7c68afe423f6b57a80a86b4655147c3cce9798cf499479c54f28d36f1f984460d687f0f93bd33946bf
+399f21ec57b664a6afa6fdd95d68b417534a0d655574de327f980ad3ae29463ab57e0f81055fe7a4967ede9216c88c29b74f
+22a2e2a9eb8a0b46fa563164792b67d61ea618188d4176a0b004fe9dcf72c18027480266f2bae911b879f67e694e54f1495e
+6c1d2b59b774c94b89fb3d85c5febd2584b6067c27d62d0c39ddd8816694583522bda386cb8e9d0f5e51ee7c75bb150243c0
+dadf0410da12ffd28c019ab9f8d2eb78d10723e5187530f5b40f78cd17394497965d73b0f41cd64d7c5ab26b3e23f4bd2bd4
+63099fb8677363d7448a8070258ffcb9
