d7ececb7973d893a3bbc663637d0e50d835e771f
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-08-17 16:08:05 +0800

    Create Problem_0138.py

---
+++ PE/Problem_0138.py
@@ -0,0 +1,26 @@
+# Consider the isosceles triangle with base length, b = 16,
+# and legs, L = 17.
+# By using the Pythagorean theorem it can be seen that
+# the height of the triangle, h = 15, which is one less
+# than the base length.
+# With b = 272 and L = 305, we get h = 273, which is one
+# more than the base length, and this is the second smallest
+# isosceles triangle with the property that h = b \pm 1.
+# Find \sum L for the twelve smallest isosceles triangles
+# for which h = b \pm 1 and b, L are positive integers.
+# ----------------------------------------------------
+# Analysis: Pell related equation
+#           Hint: similar to Problem_137
+#                 https://www.alpertron.com.ar/QUAD.HTM
+
+beb477bb43ab05314fb20326592b3870b950e976221afdecb02c248c1202e32b6d3e9948db7b6216d266e05620fa59b3fd59
+9d7e10918904cd9f15be69121951fb2b004076cc2e955b7a0059cb71254bcc03f99ed19c1d4a84739b0d717e5aa2d6f5035e
+ea533448038aab3bbc7294fb4ac9705f5b15634c8ecfa894f5d50f8696a528d1354c601fe6c3deb4e21d38ee636adf9b4cf4
+ff4a0988ee1ef9572726bf1a32c6a655b5ae1f9acc22c8d3d41a2c2d6e0dad54919b8f01a0eac08a4d76b339c83d55b506db
+058842ae7215b6bec6fb43f3306a5487f62335c4cb6705d2976f2110aea7b728d062239f01cb9fea80b46dd76d65636f61b4
+d780b73e905b7c2d2d7c311c970e351f6e587a623cbecb1f770156b7fe2b617a6a7f952807667f7326381d0ed32c3ab8a3e7
+13fe3ff7f8fee9dc0d800b4268d48b03207d32bef426f5c3f8fc574448cf82ed5d865912ea898c6b3de8d2b764c869f5aa85
+aa4f24f7ea1ebd7da5492d4b7367c3ad109559eef28fa0327613a4983706b4c6b0a199a5b0f33f91a6c6077b5b3861da45af
+7e962be4ee9f5e7c3509823321369f239d9a6aa06e5f371a84620714b4f952349ce8f6006f8f5dfa38eb65f29b2ae1e33606
+1089886a77c3a0dcc04c49e6a0cd4b696954b01224995498c4209251356c8b330779a700c51b9bb9ccd156eb439204e8daf1
+13f9a47fa166f4e918569106abf7f7cf
