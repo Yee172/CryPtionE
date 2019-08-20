351d0e3c7ec4f159a59b6131fcce2e496e390ee5
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-08-20 10:42:48 +0800

    Create Problem_0140.py

---
+++ PE/Problem_0140.py
@@ -0,0 +1,32 @@
+# Consider the infinite polynomial series A_{G}(x) =
+# x G_{1} + x^{2} G_{2} + x^{3} G_{3} + ..., where G_{k}
+# is the kth term of the second order recurrence relation
+# G_{k} = G_{k − 1} + G_{k − 2}, G_{1} = 1 and G_{2} = 4;
+# that is, 1, 4, 5, 9, 14, 23, ... .
+# For this problem we shall be concerned with values of
+# x for which A_{G}(x) is a positive integer.
+# We shall call A_{G}(x) a golden nugget if x is rational,
+# because they become increasingly rarer.
+# Find the sum of the first thirty golden nuggets.
+# ----------------------------------------------------
+# Analysis: Pell related equation
+#           A_{G}(x) = (3 x^{2} + x) / (1 - x - x^{2})
+#           Hint: similar to Problem_137
+#                 https://www.alpertron.com.ar/QUAD.HTM
+
+6eab696ef9f79b863b17f4d10ec6b0b8582f030532e57d6398f590c7ff710b1d42bfcdb869f04bad6eb7e550e0215fd01cf6
+1cc884a905c583d550a051dacafcf7a4343e64a6f37e975250af1d3ffc434ebf4b14e958a7bfbc932864df6fa8aab81f9298
+c4f841c5c410511d71629369e59f28982f22c781410fa8cac45c6945776776ea5eac7b4f0ee4023a188f48845d94f6210e8e
+7362ad87a27b96a6dc42e7737025569ff68286748b2cb549d7bd91147046aace05cc1968c4e48615d49450c33a86b27533e0
+4c0a1667ab27d74e5c4eaa4b872c039799a5c27c3d0e66f4996fc3cf6402e8e605be097b6d96323be4f069493e67d1878e9f
+1d8ebf28f7067c2d2d7c8d0be7fa0c68fa310bad867de6d1efeb4e9b3d5d7d35c822086e8c11c49275aed7602a496dbb1432
+58547ffbd788840424f9cf324481919033d83f4525063814d9075424ddb0d09bcb560d21c4c88debede32712d749b9f7fc2b
+5e3d3d7718f4769a0b45ca7ec2a5be700b0d154593b417b31be470af7448b12893f3fe9108659bd96a34c26090480b0a0ee9
+756fdc0e24e94ff92486b591fc58b8d957309e6695bd2a7500d75714670aaeb14b9fb080a92b1b68da9061bd45578113023b
+5bd901efe015b43b42f57ba859cb47f9c20971b0dfec4ff96e675d7c5debd90b955b509cbaea98e6d9173ecd3fd6cfea1499
+54faa7ec0fc84860ef9e413560f6dc5a7c2d2d7c4f66abfb7bb32f2f7af082b18e746e5a7574629e4e267e5d3b328a6fb886
+fc05d7b64437d1dc4d2f5f4f6a2285e9f342a7d1b38ad7be35d7874d5a0aafee9cad327392caf9f6f488576a292b1eb017b1
+80e09d4f3f7e58ade256b8f72b23b4eb8d4f9cc7e55dd7e78dce191012b6198ededc10185d0b31c5604d0d59a5371d181bd3
+0dca2a96df082f7c2c4da11c241c660186e0e0c0fb6d6ff30faab17f4f246ef6db38bce11448e13dafbfde68791d7ebf0266
+192339f5851dd1e47455f3f4095a76956e9f62f5817bc4474ce00f14fe49beedb577ef5af102c0fbd7fdc34f23720417888f
+c52f1d93d0681e2c5697eec46028c910d3c12e200d1e632b17a3
