a5926fec429d8bf196388a61f64a9277212d0719
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-08-23 16:32:45 +0800

    Create Problem_0176.py

---
+++ PE/Problem_0176.py
@@ -0,0 +1,39 @@
+# The four right-angled triangles with sides (9, 12, 15),
+# (12, 16, 20), (5, 12, 13) and (12, 35, 37) all have one
+# of the shorter sides (catheti) equal to 12. It can be
+# shown that no other integer sided right-angled triangle
+# exists with one of the catheti equal to 12.
+# Find the smallest integer that can be the length of a
+# cathetus of exactly 47547 different integer sided
+# right-angled triangles.
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: a^{2} + b^{2} = c^{2} => b^{2} = (c - a)(c + a)
+#                 c - a and c + a share the same parity, thus, for odd positive p and q,
+#                    if b is odd, then c - a = p, c + a = q, p q = b^{2}
+#                    if b is even, then c - a = 2 p, c + a = 2 q, p q = b^{2} / 4
+#                 The number of different integer sided right-angled triangles is equal to
+#                 the number of factors of p q with q > p (c + a > c - a)
+#                 Greedily select the smallest prime numbers to construct the required smallest integer
+
+0d3eca82ec189b50acce7ebc0db8cafe7bcac2a261e21158d8e90e9f93182544e557a96d0912438c08c90a1c4221ce17950d
+a62cb8e324f02dba5aad83783660273f03a7a6887719b230d2e22bde8bf84d1edf80a0bdc6cb828adc93aec15e2d817e5b17
+c3cb12e81b13bf17e5bd706e1e2d6d432e7b1d48a1e7a44f3a7f243bf972cdfe001d856e3d34cdbc05e3c1a697ae2d9b3e0a
+c8adc2ee5a81fe937c2ae183247e7d1080be4987a6e1dad3beea092f5fbb04bb5e301a7cf405a0d4ea8c44887605bdf03160
+d791e2463547bb02df7f43d0348e643084a55c903cd47e3092bec89aaa2e4c00c02bab8f397c100d6ac2ba944a340d555d17
+d8d8e01d427a7c2d2d7c28c0f5304cf3d0b1232851e093286fd8fa2d05ff0e59b15cc5b8f725bc291a3e69f455781b25bf49
+47003acc61d0200c25731730219733bc3567c549de17de2f1f4e3a271458db92be1541873c97362cbfcf79d0e1d6482119ed
+93f7f39942bf5d08dfcaf6d2383388888a34716e3dca70e6a708fa3f400fe4d725e0dcdd0ebae2138a6d71ac52140bd3af14
+0e60f6010711462184f2f0ccb4e5d3bc7d85d2dcf6ed05d94d89ecdbef3bd7f1a539e6789972cb78117556aca7019b1e9d8f
+f262dc278a5a7bea47b64d1d1be2e4c4d266a10a3492560f801ac6708248c0ba2a0e86e9a96b340b79e653f899b06edd2e59
+8348045bc1549e427fe1b872e797d4467c2d2d7c77a3d419114fffe7ee2be5ef588347f218ddd8c0b7a600aefb1402974402
+02399af877bc49d7655023b457b2bf9fd32d39b668656e9643259955ca672994022e0274b094b725d29426e0932a22dbb913
+2d60e7afde2c5ad6310df9da6b1f4284d4bb18e18d3dd500e5314cf748e472aad148e3cc3fd061d9f602d576c73e9cbf63e0
+47e5647e24a2a65c24c79e1eece4aeca9b27aca6d5fb7bd352bad9139e4edfc32fbc9ef25b74c0c1d0fca078232943d6af4b
+3ecd08292660981a94817d37d8f9231d40476ecec353c7f930f55fb686020c18b5666d23bb97465c5e72db51130ec5519f7a
+56fa212b0bf623d56289a89e3a24a3ef3a54d818768591b27f117c2d2d7ca75755c5f97151e889786805ed64b8da817a6a8f
+446dee4cf7610ddca9792cd40856baa2097f540e1542639b60108271a2fcd7a2c5af67cae25c2b651689e7f88b963558ae56
+e9c45bd9f0a23e30956870d013130538c3c1ccadf71151235bb6014f8255abc61898c225977b83c9f2a6dd4f68d50a7002bd
+8ce3e557635bf694749dc9bf7dc8af1b53f3d8e366c4f8cd640d4d34d9b5c628a8545d8ddae79dc8ee0f0c2435a05b48ff94
+80982d6c0b1f46319e6cb638f1fdf57773f4ae868a8b82808c1dfe517e22d3eff8b5cfd29f3bd28b2fd84a5c17930de40306
+00c705028556db49ca2ac39dead26b62fff8f421a9973a234aa1a19a8cbf48d476fa72d4
