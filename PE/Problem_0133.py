4f42c670490112141905da61d56760198325aa93
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-09-14 10:03:45 +0800

    Create Problem_0133.py

---
+++ PE/Problem_0133.py
@@ -0,0 +1,25 @@
+# A number consisting entirely of ones is called a repunit.
+# We shall define R(k) to be a repunit of length k.
+# Let us consider repunits of the form R(10^{n}).
+# Although R(10), R(100), or R(1000) are not divisible by 17,
+# R(10000) is divisible by 17. Yet there is no value of n
+# for which R(10^{n}) will divide by 19. In fact, it is
+# remarkable that 11, 17, 41, and 73 are the only four
+# primes below one-hundred that can be a factor of R(10^{n}).
+# Find the sum of all the primes below one-hundred thousand
+# that will never be a factor of R(10^{n}).
+# ----------------------------------------------------
+# Analysis: brute force
+#           Related with Problem_0129
+
+7c5dd8c63624e743189350fcb86319f97300083e6ad9158844c202c1c8caa423e4ff70c329f3db034523ee1367a0b8039ac3
+1f6764a0cdf834cbb448d3f2788c1548c57da24b2234f5c66e5c97cb026cef4cd70985850adb4297c79193e78e83c888e8f1
+eec114b01aa3c57ce11b0b7161363ec3853b36b14ace21af9d3e02656e9343891b5959e8b98b1d3a3689c7f7d2b65c779597
+966a176d667cfd7705d57f283ec1383889a4dd1a9ef7be3f7699b19fd1d42338a0c96c357c6dd9b7bacbff1da3874ad3fbe6
+0df9e511cf67860dc8e6f3ecc54089990d1c19cf385e0f04b7b821983167a135f0cd2d323e383704f4209c31040118c48ae3
+0f941c31ca367c2d2d7c8f3852ce78496004a752cb9d72d4d59ee5ba022397661d00637791229c0910393f74d942e7a1efe6
+7e45facc95a78993408a4f6c5a1e59c9958f676a1c8669dddb2466142de3f06f7a03c27c1ce32de260679ed4648ba5ec1607
+78c672e199e578fd961013552f80d74eb297de95ebf6de4789c88ecb10a570d35313849aefa0857278c922482f795b63188b
+cf0fee9d53466a3e53aa28f27b27d571490c71c63e729e6f6c9d799b2a95639ffcde02de54f4388bdfa29cc006e91923fe9a
+00b629987c9355152e40d501e3d7b9bdeb787f959781425144ccd0f63a968ba88f386d0034df20fc02959f71492d72d3ef29
+56caba6b39ac7d082c2e11d6e5ff57ce
