02052d48811da12b9b033fe598c508ebbeeceb73
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-09-18 13:58:54 +0800

    Create Problem_0239.py

---
+++ PE/Problem_0239.py
@@ -0,0 +1,38 @@
+# A set of disks numbered 1 through 100 are placed in
+# a line in random order.
+# What is the probability that we have a partial derangement
+# such that exactly 22 prime number discs are found away from
+# their natural positions? (Any number of non-prime disks
+# may also be found in or out of their natural positions.)
+# Give your answer rounded to 12 places behind the decimal
+# point in the form 0.abcdefghijkl.
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: similar to dealing with derangement
+#                 consider swap the first must-misplace one with no-matter-what one
+#                        + swap the first must-misplace one with another must-misplace one
+#                 both swap operations will eliminate one must-misplace require
+#                 swap the first must-misplace one with another must-misplace one will additionally
+#                    transform one must-misplace require into no-matter-what require
+
+18e0a04af4fcf7294b4b96d730649a973c3f52bc589ce92b004dd91f21c888f539c58e217be554f71ad72155dc52e1314838
+14b7955c9ade3721e0513ae73de87c6367e327c456d49cf412e6ed3e26fb1113a7ed478f73e9e46a59764799c97a73771fa0
+c5e885db3082beb4f9a4d34ec240ad54d6f67ef0566f4109c491c1a050796a0ccc844759c0d0f4475d64561dd9649211d7bd
+d4779ea6b1e452015419988f6d80c3cfb4c95aedcdc29925d0aeac9449562eab9042993deee5c47c6e5ea53d33bd79c0ce48
+9ad59050ee1c85b8529669a12f1801de624a232f7667ef7129c64e6475a971b14d403716dbb1dc5596d0eadaa8e04fddecac
+66ffd5bb4ddd7c2d2d7c70a730a313861f0519e7b9f3d3c96d2f149b7934d235be48af87bf91939b9e65523d2ef104790c3b
+9de4bbe30a21540dd41175428a86c4d65a2b65e3e841681cc0f85baf4ded4ed28572a22423563d9faeecb9edc45c19d94174
+b7de3e449664314b9ea84fffe51ee7e3682d5a47352b081a4a7ddcf82726f3a3b3602a2c0e9cdfce836ca72a00ec3144bb4f
+41e0b76499768ec0b23ad38cf074322b76e3931c9f5fe08d7be5614fe3e8a48169042e76b3b5fcb995d50383c2107336707c
+a08498783167a904ee2766a08c2a80bd88fd5ebd43c18aeccf890fa10bc942c30c26f034e354f4da0b9c6466c1ac701dbeca
+d19f9bd1a0ad86e3a98f3e9ec70f31ef7c2d2d7c2f0c3f15006093d6152dc429f85ede0065cbc498d35a4c0bd05d275a5546
+6f33419d0c34fad80ee1e29832fee00ea7da0bc21175817bbca724f8563fe0e4a348bd4f0e58ce0371d4d10a5027c1ddec99
+6d23ebed1aa0421cd2a0b154a98955ee79fcd7f0e39095c1359f78511de3de47589b3ca187cb193fa39c58e181a9c1ec0c3c
+b9f7357a0c9ef87fcbaf69050c7e135d376c610d16067c2fbdbcd348822284fd93ff8f1228904462c0d45daa6b5a945a823a
+b7de4b25f3106fa7e9745a17c157816c93971305e4033e024932e83dc82f49fa76f8e2c475479e4983178afd83e53aabf700
+e9e3bd0f5eac077b55b263312f453cf77c01b732129d74f8b00e7c2d2d7cb61bcdb455bdb6bdff1e5473c6cac7902ca530b8
+296d0a404c755592dd32e4f37dd0bf5274f22717b16ae4e4a02d7dd29aac493b7440a3564bc362515310e69bea1c2f7f4c16
+0e60059e5c8173811856ddbb5fbd8afd87fe986f3c460d345740f7c832148aea47acafddedda389e61bde0b0776548be7298
+b6963b44fc13a3f433e9c7049064a40a42c334686bf6a448c5689366876a2055d570397917906e17b17dc91d1951a0b3c9a7
+448679739ff8bc93078614ed75fcbdac337b9010bd0f0e1ecb831ed3d1ee8e1e4804442d4f98216f2c46852357409a72c5cf
+d3077f1a31908673dfcec0821ff1dd389cf22245be5c38efd1a838b9e016887cb2b1e197
