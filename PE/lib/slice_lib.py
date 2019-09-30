655a22dedccdad503c9a09dff55a32cac9ef9ec9
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-09-30 20:12:56 +0800

    Create slice_lib.py

---
+++ PE/lib/slice_lib.py
@@ -0,0 +1,24 @@
+"""A library for slice
+
+This library is set for slice
+"""
+def rearrange_slice_for_infinite_sequence(s):
+    # [minimum, maximum)
+    minimum = s.start
+    maximum = s.stop
+    step = s.step or 1
+    reverse_flag = False
+    if step < 0:
+        reverse_flag = True
+        step = -step
+        if minimum is None:
+            raise ArithmeticError('could not deal with an infinite sequence')
+        else:
+            maximum = maximum or 0
+            minimum, maximum = minimum - (minimum - maximum - 1) // step * step, minimum + 1
+    minimum = minimum or 0
+    if maximum is None:
+        raise ArithmeticError('could not deal with an infinite sequence')
+    if minimum < 0 or maximum < 0:
+        raise ArithmeticError('could not deal with negative index')
+    return reverse_flag, minimum, maximum, step
