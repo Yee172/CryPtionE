f811b320a84b84cc80ca7f5613c47faaa06aa83b
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-10-04 12:17:57 +0800

    Update Pell_equation.py

--- PE/lib/Pell_equation.py
+++ PE/lib/Pell_equation.py
@@ -5,18 +5,26 @@ class PellEquation:
     """Pell Equation
     
     x^{2} - D y^{2} = 1, where D is a postive integer
-    solving for positive integer pair (x, y) satisfying the equation
+    solving for positive integer pair (x, y) satisfying the equation with the minimum solution of x below the upper bound
     """
-    def __init__(self, D, modulo=0):
+    def __init__(self, D, x_upper_bound=0, modulo=0):
         if not isinstance(D, int) or D < 1:
             raise TypeError('D should be a positive integer')
         self.D = D
+        self.x_upper_bound = x_upper_bound
         self.modulo = modulo
+        self.solution_out_of_upper_bound = False
         self.minimum_x, self.minimum_y = self.__get_minimum_positive_solution()
         self.no_solution = self.minimum_x == 1
         self.transfer_matrix = self.__get_transfer_matrix()
 
     def __get_minimum_positive_solution(self):
+        if self.x_upper_bound > 0:
+            return self.__get_minimum_positive_solution_within_upper_bound()
+        else:
+            return self.__get_minimum_positive_solution_without_upper_bound()
+
+    def __get_minimum_positive_solution_without_upper_bound(self):
         s = self.D ** .5
         p = 1
         q = int(s)
@@ -31,6 +39,26 @@ class PellEquation:
             if x ** 2 - self.D * y ** 2 == 1:
                 break
         return x, y
+
+    def __get_minimum_positive_solution_within_upper_bound(self):
+        s = self.D ** .5
+        p = 1
+        q = int(s)
+        if q ** 2 == self.D:
+            return 1, 0
+        x, y, last_x, last_y = q, 1, 1, 0
+        while x < self.x_upper_bound:
+            t = int(p / (s - q))
+            p, q = q, (self.D - q ** 2) // p
+            p, q = q, q * t - p
+            x, y, last_x, last_y = t * x + last_x, t * y + last_y, x, y
+            if x ** 2 - self.D * y ** 2 == 1:
+                break
+        if x < self.x_upper_bound:
+            return x, y
+        else:
+            self.solution_out_of_upper_bound = True
+            return 1, 0
 
     def __get_transfer_matrix(self):
         if self.no_solution:
