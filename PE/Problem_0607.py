4cb01007f93894ac882787536c7152f10564b74d
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-10-06 12:41:00 +0800

    Create Problem_0607.py

---
+++ PE/Problem_0607.py
@@ -0,0 +1,51 @@
+# Frodo and Sam need to travel 100 leagues due East from
+# point A to point B. On normal terrain, they can cover
+# 10 leagues per day, and so the journey would take 10 days.
+# However, their path is crossed by a long marsh which
+# runs exactly South-West to North-East, and walking
+# through the marsh will slow them down. The marsh is 50
+# leagues wide at all points, and the mid-point of AB is
+# located in the middle of the marsh.
+# The marsh consists of 5 distinct regions, each 10 leagues
+# across, as shown by the shading in the map. The strip
+# closest to point A is relatively light marsh, and can
+# be crossed at a speed of 9 leagues per day. However,
+# each strip becomes progressively harder to navigate,
+# the speeds going down to 8, 7, 6 and finally 5 leagues
+# per day for the final region of marsh, before it ends
+# and the terrain becomes easier again, with the speed
+# going back to 10 leagues per day.
+# If Frodo and Sam were to head directly East for point B,
+# they would travel exactly 100 leagues, and the journey
+# would take approximately 13.4738 days. However, this
+# time can be shortened if they deviate from the direct path.
+# Find the shortest possible time required to travel from
+# point A to B, and give your answer in days, rounded to
+# 10 decimal places.
+# ----------------------------------------------------
+# Analysis: ternary search
+#           Hint: write down the travel time formula according to
+#                 the angle of marching routine in each region,
+#                 then partial derivative it
+
+44af53ace80b91965e68249cdb45fb9bd97225bf956c5c561476b3f138890cbfd1382811e2706dbe6977b4da042aa2be2103
+2c5f05611cbc24c85e963bb5e6509ce3672c20df69b78e4e674e89069d064e993232b0066a38722a0ee903335e376b2e31a7
+7e00e3269ab4f8eb4708019d2f72f5cf9a2e7cade58d6231f74dec5ece3659bb89c5f3900132771de3d7642d1c68ed4890b7
+ce99261267e2954a8bdd7e06c09d42371292dc04aae737261a4984a1a305740aafc5b810aa54ad82b0efa93e1fe7e005f4b6
+ecd740d4713fd335ac69e4057e14b52001077a74b8ef735729c36282c19966adc48754fb3535ce54e31acadb1ed5fcbb9e2f
+9aebb187dd697c2d2d7c8827b75256ef1066a374a9577ab1daf7bd7630d8ef0ac19c476e4634cee3818f40cdc54736f70fc3
+12e5a7e8137b586ae0b2116124a4224410fe8d1ed28dc8c412a4899f24279a2d5a2150669751bbf0b2632703bd9bb2fba365
+5180ca08d01f417bdd217faa582cf190f78f7ef0a4dee79de03efd345c7ba124846b7d0d43456e400a8d88fadd49c1d77d4f
+33c393d70827b1f3aa4482a4e9f0a23c64f00b2fd3cd3443e9af86f98548d122f5b78b49205b7d48329184d25907a9991a12
+766ca7bd2a932faf9b644cd6285b02c3dab367540375294fd2e94da90216d840da3455a7e9005bda3f962a5da3c0c6b6135b
+903b47de766313642e66c83db0a9b2b77c2d2d7c675a4376ddd2e216c08b7034860180f67056b1c03355a6f260b4be4a815c
+a1838d524595b708239e9b4991b0ea01e55555fc4a306deffb142f2e48dcba8030ee95a4ab4e072f00410c0887fb6e810bce
+03541bbd27c54eb7bb804ab7fdd1879acec1fbaa918cf23e1f129aca9b59cefbaf0fe472451564e5817fe740cf002a13bbf3
+0257ab4e36400ed05af395d157d634d330b0e1fefe78a7de64b64141736d83a1f0068699af050dcb58a34492d3094df7e3b0
+77bb45bb7004f1629c4344e729faff74123f20937870c9a1eaa68d28e6785016239c15c930b2f7bcced0e430e6a0406a0170
+f7a0499b0800727d0d31612ba55f9084488f8ef4269905861db27c2d2d7cc5be961c07ab2dc3b33ce443e7f1c9561a2b032d
+581d14e62e545a2dae684ae24f29c0fdc9d780fc938687a24fc5803f2c6eb227ae155ef50b5e7a222356a7a77b1b6785c5c4
+bacc47eb4ba9712f33fb68dfb5f217019bf65a5441292081c11338a3dad3008c97efa3523124beb013a4dc9fd80f31fba908
+ea6aba9bf387b9be143ed8259103b69b0759da32c10805438c44e40fe98aeb98045582efcc2cf378d0fa8f02ccd74fabb451
+745829b55922f357ecdeefa554517ed478891e63045dd9a1207cd978641d4dd990876e27c2d9cbf77da16102ac05da291e97
+76badd480aa107cc55dec03c471f43368230aa7e0860dc37a5fb9055d479b32fd30fbe52
