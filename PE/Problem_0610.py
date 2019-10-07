fd6528e72032750deaf78ed0f8b019de22222a62
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-10-07 16:13:18 +0800

    Create Problem_0610.py

---
+++ PE/Problem_0610.py
@@ -0,0 +1,50 @@
+# A random generator produces a sequence of symbols drawn
+# from the set {I, V, X, L, C, D, M, #}. Each item in the
+# sequence is determined by selecting one of these symbols
+# at random, independently of the other items in the sequence.
+# At each step, the seven letters are equally likely to
+# be selected, with probability 14% each, but the # symbol
+# only has a 2% chance of selection.
+# We write down the sequence of letters from left to right
+# as they are generated, and we stop at the first occurrence
+# of the # symbol (without writing it). However, we stipulate
+# that what we have written down must always (when non-empty)
+# be a valid Roman numeral representation in minimal form.
+# If appending the next letter would contravene this then we
+# simply skip it and try again with the next symbol generated.
+# Find the expected value of the number represented by what
+# we have written down when we stop. (If nothing is written
+# down then count that as zero.) Give your answer rounded
+# to 8 places after the decimal point.
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: notice that we can have infinite many Ms in the head,
+#                 so we only need to brute force the expectation from 0 to 999,
+#                 add with 1000 * the expectation of how many Ms are in the head
+
+b90a710ba4812207c1e20ed42d0799119a32b00a07dac8e31521fe9b4be5d80cceb1bbf65c11819ab854a20948020eb58e2e
+bd04b2084b47feb8798ef230a5567f772d989f4cbf43534b82f99505bb5d3e3b6153631347381b02d8d4af39390507c44490
+3b2d4dc12caee08a773d1086d318a6aa782f58643fe1a47b1207e1db77f0d982c3cfe315c7fb94625ee338c57fa1c7ba0692
+b5ea089d9b72be3b90deeaa9ddfc960ca9a1bdf3b89b1c8f728507d8b84e31845194a2e7db492a15f6dfac373d5456059783
+866192e00a2074bd40169329a8dc66f770ae8b421129b2e695f11a7dfea278d3ee34534a6764125c3b0aad55dbb39493e10d
+8440c14270697c2d2d7cbbdec3eb8574cb0554f1eaeae2412acd841afeebc2c616ca8129b2ce532b9b0b6a1f89de798d3cfb
+a6e6bfd928675f37ba2d426aa6f6f38a8f4f9dd70db1dcba7ffc1644ea15e7a616de449a016799fc27d149eab5c48a963534
+7019b1f147eac2e0e6d4a5dbe25a80da7b6a8f60fd04d67193f04a5d17738f183ae70751b26d12004f4aa7e1a1a4bff5158f
+127704cd88c98777288159a7d3d3bc0f22233839ef78c74302c9ab598f4d408407dabcb7184a9c81c35d9af5eb42d7e33d1b
+6c098001483555384cea4e07850fd0b459a8d70832dacbf9f02822d479fb96825a3269a2d416b484317a77c81d1ece6ac0ae
+2eadf83456becbbfd375f8184d3f407f7c2d2d7c487227baafda3e488b27178d107aad65f00469da6d3efbef253db400a59f
+bad0bc945cc0923ac0ca3680532c80c5e22b7e37018a1de70783cfa6034b5104875ee2d32226ebf5fc8c1283d75e18992967
+8001addc69be2bdb42048e0d23ba99bdc14eb20346d4adc0ac940928ff7cef42d4129af60214910d2f70e8a3c60cf25dd5b4
+5187d82283f7bf13173cfa091f0fe30b55fb7f8183050247ff9cd49afb749ea4dd8002f65a4c432a2ec949d8783620ebb25c
+7ea6fbade9dd0f11d7d017502929b0b00b7b844b21c2df7da145b3e25448dfa3d2c269a986d55666399ba7c4706cb904cc89
+62c721edf479023cb9ff78ed75c59b664e4cb3ede5e6fbad124e7c2d2d7c1333635995db20ac8ccef075e56256609a066cd1
+f18a63e40adf47f55e63dfec60e1dcfbdc066764ba7f96473e4c19338c75527a3a34bfee612c02caaf799b87c0f943fcb42d
+3c21f816a1db1290f7bea36f432112ecf1c875d036594aa205ad02e1efe6a0213d3f772fd6927cb7aeb8af942881cc707592
+80fe354c1e04f83ccc47f435387e568ba5040394ddfdab5bfbb53770696d91aefee1412d4c70c202f3d3936dd1febc6e2140
+e9b2e6c260d0f63f757e9f7c56a45853e55d64075845e52c6ca158d7e281b602d658ebfd4d138f64a4998eb34be3532a4ee3
+fba6115310af5e232513dbd3cb381238398231deea474a71efde2dc4a364fec1afc156bf7c2d2d7c1439831d5e1c2ba4944b
+876365e3f6287b4653ab370ba0dc2f3e25acc28c1d418b58676499844885b3a6f11ddb7383a00ed8141138375bd1ef7fe807
+54945218d8739156e323a1438dd03186d9c60b06ad576f736060b0868a25940a0aa9e6d5f7fea1ede597cff333bb2eb0ff96
+b706c36c4d92155db439ed08e4a0821fbec12f9de7f7313ab9836d661288ed36d6a739c3de193a8f568c253a94a34191e23c
+ce24372d3230e3e19d8da6cf8cd725b7d5122035d585cf085465e1c5ccabecccd266aa3edd17fb1d2d81a971afd9d6ca28fc
+33d09b38c993db4cdaf0bc8b6055156a9f9d5d03fe77ab71694fc535626f3402dbb7372883ec9696d6c1b6e05c28
