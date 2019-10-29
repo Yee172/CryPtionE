110d53621dffa67cd800448ab0f880236857d766
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-10-29 11:29:06 +0800

    Create Problem_0258.py

---
+++ PE/Problem_0258.py
@@ -0,0 +1,13 @@
+# A sequence is defined as:
+#    g_{k} = 1, for 0 \leq k \leq 1999
+#    g_{k} = g_{k - 2000} + g_{k - 1999}, for k \geq 2000.
+# Find g_{k} mod 20092010 for k = 10^{18}.
+# ----------------------------------------------------
+# Analysis: linear recursion
+
+826178800c09d9929d622870348db77b850d1161d965acb459b0ca79e6d9ec0acfb00ceee4bfabde2e0ee76d45467f4c394e
+fbe40341ff9ee404c3d6f218b19ee63a3d0b2f00173fbb109403d31e0527ef25329679d317877390461c09913b6c889fa166
+258eaced058cf9f785872b2cf99560ceb9c975525ce650fee7ee01d329db166da0ccf57f30766bdac26b5fd948e8e65ddf60
+3f7a7d544851f4b17157072cdbac44c7bf05aaed1ff763f0b1d8769fc44d8f56d01979c265f6d5b41c7130c291c3e046e962
+6e3a664a54e977c1ea33d3fa95e489c9c664e93dad535c0772aa36ec0fb4cb9c4d016cd36951a24945e393149eacaaddcc28
+a5f9c27360ac
