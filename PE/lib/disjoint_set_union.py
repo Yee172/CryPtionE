0613921e9a73edccb2e7303e7b342d55d2889050
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-10-30 10:48:27 +0800

    Update disjoint_set_union.py

--- PE/lib/disjoint_set_union.py
+++ PE/lib/disjoint_set_union.py
@@ -1,4 +1,4 @@
-class disjoint_set_union:
+class DisjointSetUnionPrototype:
     """Disjoint set union (AKA dsu)
 
     Basis usages and functions
@@ -9,7 +9,7 @@ class disjoint_set_union:
     def get_father(self, x):
         """Get the father of index x
         
-        get the father of index x and compress the route
+        Get the father of index x and compress the route
         
         Arguments:
             x {int} -- index
@@ -23,10 +23,56 @@ class disjoint_set_union:
     def union_father(self, x, y):
         """Union the set contains x and the set contains y
         
-        union the most father of index x and index y
+        Union the most father of index x and index y
         
         Arguments:
             x {int} -- index
             y {int} -- index
         """
         self.father[self.get_father(y)] = self.get_father(x)
+
+
+class DisjointSetUnion(DisjointSetUnionPrototype):
+    """Disjoint set union
+
+    Same as DisjointSetUnionPrototype
+    """
+    pass
+
+
+class DisjointSetUnionCounting(DisjointSetUnionPrototype):
+    """Disjoint set union with counting
+
+    Basis usages and functions
+    """
+    def __init__(self, length):
+        super(DisjointSetUnionCounting, self).__init__(length)
+        self.quantity = [1] * length
+
+    def union_father(self, x, y):
+        """Union the set contains x and the set contains y with counting
+        
+        Union the most father of index x and index y with counting
+        
+        Arguments:
+            x {int} -- index
+            y {int} -- index
+        """
+        x = self.get_father(x)
+        y = self.get_father(y)
+        if x != y:
+            self.quantity[x] += self.quantity[y]
+            self.father[y] = x
+
+    def get_quantity(self, x):
+        """Get the quantity of one set
+        
+        Get the quantity of the father of index x
+        
+        Arguments:
+            x {int} -- index
+        
+        Returns:
+            int -- quantity of the set contains index x
+        """
+        return self.quantity[self.get_father(x)]
