fac5faeb0f2343c6ebc6176ac374ab809e320e59
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-11-13 12:20:34 +0800

    Update complex_number.py

--- PE/lib/complex_number.py
+++ PE/lib/complex_number.py
@@ -1,34 +1,85 @@
-class ComplexNumber:
+from math import acos
+
+
+__all__ = ['ComplexNumber']
+
+
+class ComplexNumber(complex):
     """complex number
     
     common complex number implement
     """
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    @staticmethod
+    def _get_real_part(complex_number):
+        return complex_number.real
+
+    @staticmethod
+    def _get_imaginary_part(complex_number):
+        return complex_number.imag
+
+    @staticmethod
+    def _get_norm_square(complex_number):
+        return complex_number.__get_real_part() ** 2 + complex_number.__get_imaginary_part() ** 2
+
+    @staticmethod
+    def _get_norm(complex_number):
+        return ComplexNumber.__abs__(complex_number)
+
+    def __get_real_part(self):
+        return self.real
+
+    def __get_imaginary_part(self):
+        return self.imag
 
     def __add__(self, another):
-        return self.__class__(self.x + another.x, self.y + another.y)
+        return self.__class__(super(ComplexNumber, self).__add__(another))
+
+    def __radd__(self, another):
+        return self.__class__(super(ComplexNumber, self).__radd__(another))
 
     def __sub__(self, another):
-        return self.__class__(self.x - another.x, self.y - another.y)
+        return self.__class__(super(ComplexNumber, self).__sub__(another))
+
+    def __rsub__(self, another):
+        return self.__class__(super(ComplexNumber, self).__rsub__(another))
 
     def __mul__(self, another):
-        return self.__class__(self.x * another.x - self.y * another.y, self.x * another.y + self.y * another.x)
+        return self.__class__(super(ComplexNumber, self).__mul__(another))
 
-    def __mod__(self, modulo):
-        return self.__class__(x % modulo, y % modulo)
+    def __rmul__(self, another):
+        return self.__class__(super(ComplexNumber, self).__rmul__(another))
 
-    def __pow__(self, exponent):
-        result = self.__class__(1, 0)
-        if exponent:
-            temporary = self.__class__(self.x, self.y)
-            while exponent:
-                if exponent & 1:
-                    result *= temporary
-                temporary *= temporary
-                exponent >>= 1
-        return result
+    def __truediv__(self, another):
+        return self.__class__(super(ComplexNumber, self).__truediv__(another))
 
-    def __str__(self):
-        return '({}, {} i)'.format(self.x, self.y)
+    def __rtruediv__(self, another):
+        return self.__class__(super(ComplexNumber, self).__rtruediv__(another))
+
+    def __pow__(self, exponent, modulo=None):
+        return self.__class__(super(ComplexNumber, self).__pow__(exponent, modulo))
+
+    def __rpow__(self, base, modulo=None):
+        return self.__class__(super(ComplexNumber, self).__rpow__(base, modulo))
+
+    def __neg__(self):
+        return self.__class__(super(ComplexNumber, self).__neg__())
+
+    def __pos__(self):
+        return self.__class__(super(ComplexNumber, self).__pos__())
+
+    def __getattr__(self, attribute):
+        if attribute in ['x', 're']:
+            return self.__class__._get_real_part(self)
+        if attribute in ['y', 'im', 'image']:
+            return self.__class__._get_imaginary_part(self)
+        if attribute in ['r', 'modulus']:
+            return self.norm()
+        if attribute == 'theta':
+            norm = self.norm()
+            return -acos(self.__get_real_part() / norm) if self.y < 0 else acos(self.__get_real_part() / norm)
+
+    def norm_square(self):
+        return self.__class__._get_norm_square(self)
+
+    def norm(self):
+        return self.__class__._get_norm(self)
