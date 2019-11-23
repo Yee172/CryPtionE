a90ca4a3016c6b38b5a0bca56424ef5f4789161c
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-11-23 14:39:45 +0800

    Create Problem_0210.py

---
+++ PE/Problem_0210.py
@@ -0,0 +1,21 @@
+# Consider the set S(r) of points (x, y) with integer
+# coordinates satisfying |x| + |y| \leq r.
+# Let O be the point (0, 0) and C the point (r / 4, r / 4).
+# Let N(r) be the number of points B in S(r), so that
+# the triangle OBC has an obtuse angle.
+# What is N(1000000000)?
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: the hardest part is to count the lattice points in the circle
+
+b4de689dcdfeb30095a8b619b7dd3d17f5e2fae00053ef2c7bcbe6003e9aae11a6df52bd8b0fbe9bd1984447f9d6ce5df3ab
+f7fae477020d0e5d6e04529799813d6936fbd862e635005ff69ea6737162fb4ea7fe2e6bf77f3ac40b12cd6413545dc3ac7c
+63fd682938db55286856a3e3ce93d7c1869251ed10eef4d9adc7e1685541c7beace8d86ee5b7ef62eafe9085bbde9e6d141b
+e4c40aaca656188b691367e135190895eafb84816c8e4475feeccd4e99b859b0fd7e5612173a08987c855adb73b1e47ed2c8
+14c26faf6ff57200955ecd67a75641419b9d6692ad645bea50ee79119103fd26d364b68b61c7910bb750726d8f71136b07f6
+58fe5704c6937c2d2d7c1da5a4ba0c8092604d203d183e0cf3c674dbf1b8f3494812d26a3b715efddc18080e046710ceebcd
+5932f1c0a39012a4f41fe0df1ff96ec2ea4b5fd7301e7ab9d4f9465abb68e35d5a9060b8f9d812cf8183a37fcddd6e728a40
+43c439fcd7233fc95bac87724abf93785701e2efe2ff8c5b9f0f36c11f18e78347d624c54965edbb037f7bb18ae29c3a48fd
+835abd63d78c952b5d65cf70eefb620909bdd19484a13637fab358cdcae12fa4766bec141b4104b7251dc6bab9ffc94f4504
+71192c8aea3dc58267fe656c54ff42c22681203991947808f9adf8d81c8f65da40ff5ba9588eebb035f0e9bdeb5c31aafef9
+4411690f551a56a5d36616d9ef1a8ab2
