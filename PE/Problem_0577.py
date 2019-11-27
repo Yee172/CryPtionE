572359a3d9c38197d383897b9e3cbc207442d7b8
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-11-27 15:02:00 +0800

    Create Problem_0577.py

---
+++ PE/Problem_0577.py
@@ -0,0 +1,18 @@
+# An equilateral triangle with integer side length n \geq 3
+# is divided into n^{2} equilateral triangles with side
+# length 1.
+# The vertices of these triangles constitute a triangular
+# lattice with \frac{(n + 1)(n + 2)}{2} lattice points.
+# Let H(n) be the number of all regular hexagons that can
+# be found by connecting 6 of these points.
+# Find \sum_{n = 3}^{12345} H(n).
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: https://oeis.org/A011779
+
+b98fe33e676847adaa5558f0497b4677c039d88b33f02ebd34abd988789bbe719f5e7d8d8e710aeee86fceb1b7ea78a244a8
+8b2113a3dfcd71beba5cc64a3b9b051d6298647985320604231c622d6ae812b9a1cc03fd178df9185863f3ee94a327e2c545
+38e0df9f0b9b9ac972b78f6ef88cc8dd5b44e4f56802d7d716357356d0fc9b74db29ecf804776d9f1421488e770dd7097fbd
+3a7729563ac277f9a42ad9daa02c04f883f659938a136d88dd93c9c92c11b3c8899b4f791985dda3cf8e9186abc3ca986585
+982182c8e9506a104434984989a12c7a0fd800ec7d3cf93abb2de7870a8a3e29a2dbc1b6c71b87f5a300ab3b56a7d40a0dd2
+81beebba3e13
