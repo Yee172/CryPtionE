27e8d92f989ef40f09361ee4456a333e22305c1c
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-11-30 15:01:47 +0800

    Create Problem_0335.py

---
+++ PE/Problem_0335.py
@@ -0,0 +1,20 @@
+# Whenever Peter feels bored, he places some bowls,
+# containing one bean each, in a circle. After this, he
+# takes all the beans out of a certain bowl and drops
+# them one by one in the bowls going clockwise. He repeats
+# this, starting from the bowl he dropped the last bean
+# in, until the initial situation appears again.
+# Let M(x) represent the number of moves required to
+# return to the initial situation, starting with x bowls.
+# Find \sum_{k = 0}^{10^{18}} M(2^{k} + 1). Give your
+# answer modulo 7^{9}.
+# ----------------------------------------------------
+# Analysis: brute force
+#           PS: still don't know why
+
+9a29469940c115e19cec7a1ed486b7ef6382b074a7ded21dac6d574a5c33637a07a8c570cbbf79414f5f7f3ca59acd19695d
+2ce400c2e4bd2b374ea9e21aeba382e12c8aede28caf93cb3dcea526199141a7a95e2d39001e5f593f15a464878f32dc595b
+a6064fbc2cdb8e6c547cecf74579f094569b63402f1c62c57e9e5e46c544388f94b7e5f1b55d337f035654740e58ec7e842b
+3733ce300b9436fe0de73828020ccef3196746eeb0a43fd6569ea79ac29f6d6a0730eecd4ed53fca080107232ed286f54111
+142d582f0ffa9b34a50c17bd8daebb4048832f8fae2b3ab7d049a015ef355b4e75d96f3d9d5efa4f240d3b4189a15034700f
+e2bc834828eb
