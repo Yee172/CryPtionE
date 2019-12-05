53ac024009a0df62850cfcc8a4a8a3da0e2d83e2
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-12-05 18:13:56 +0800

    Create Problem_0284.py

---
+++ PE/Problem_0284.py
@@ -0,0 +1,43 @@
+# The 3-digit number 376 in the decimal numbering system
+# is an example of numbers with the special property that
+# its square ends with the same digits: 376^{2} = 141376.
+# Let's call a number with this property a steady square.
+# Steady squares can also be observed in other numbering
+# systems. In the base 14 numbering system, the 3-digit
+# number c37 is also a steady square: c37^{2} = aa0c37,
+# and the sum of its digits is c + 3 + 7 = 18 in the same
+# numbering system. The letters a, b, c and d are used
+# for the 10, 11, 12 and 13 digits respectively, in a
+# manner similar to the hexadecimal numbering system.
+# For 1 \leq n \leq 9, the sum of the digits of all the
+# n-digit steady squares in the base 14 numbering system
+# is 2d8 (582 decimal). Steady squares with leading 0's
+# are not allowed.
+# Find the sum of the digits of all the n-digit steady
+# squares in the base 14 numbering system for
+# 1 \leq n \leq 10000 (decimal) and give your answer in
+# the base 14 system using lower case letters where necessary.
+# ----------------------------------------------------
+# Analysis: brute force
+
+afcc591faec289ba35404cbfe833cb7c8986b816c0b6875b51c6351dae4c1882a36cf52f7cdf77d5cc28cace4c8f995974cb
+efd2c7ef6c42268341218fa19c6640a0a50c6ce832ceaaf39a00890282c4c874f4d004df887d4eb16452b0acd47865a14127
+c39b369908857458177acd83a324bf7dc15b66d66f139956535d70ab066494b346c43a123f7936c235c0b75f6ab1f395dfc2
+da3dcf4c67e70d99c2784266e457c6e30b537a1b427f70391bfb80fec0e20d7d412074b0f939ca3a053b1b01a957e5736a06
+dc4bf88566a7251cedd9674ef53a15ac806ff4b8d3771b5042d7cae98d1418173617dfc5a5c2acca8e41a173834925e17100
+aee8b0e556fc7c2d2d7cc4957d6dbc935d5d349045e3e75b51e61e68ca7f81d60f995367be3fcf037ff29944429f5be90d1d
+bd6e7c864e8755d86d22aae2ff0fa76678265ab6b3223f5ba5015414c522e8f22af3910a06c6d674b4a861041dcafe4d01a4
+4aef4f5ab67866fc9e5881b49ef30ea8f0335cf6fe5236d0ab7fa1b1ff12df91abc280b17da44171acc6d907ddaeb9b1ae82
+ac71ab968ca31a30218a39aec34a2bdf43e5c4a7619b12337acb6ec917e1cfcbd60135c65a546f5962bcf0f278ff505147e1
+1fc16256f882db8317b175d04f56fc05df0dcd8e694cbd2ec7a8e27bd8c91d3742d5e6d6e5a1171f74b9da2f9097469ae318
+f5e2c3bfe46f43b41f356d5fd33cbb797c2d2d7c41d1b810d4273a3d35543f06807443fad544ef777530cd051caef16fcc57
+2c97ef8264a43fba93db83025d0ee014227d4430db52ed9073ce9286f5b70f8cf42cb80812cc2cfa69483bd097a6bba9f2fb
+7afa51d16b89da5ed840ccb135679ba18f26567daa28f444e6d78bf68e5f7d09c0a09f7662925672fb184041f40f668d7e33
+9b75baf13c6fe5de2ad01a189d3e6f083e623809f89ee830e1f82c18c3b762531853eed0647c4ece2513a96593b0dccf9a26
+9f4a39a20d787d7527b9ed3d7a40bc2493e49977fd0382a2a0e8c03747340ec92b8b4c88ffb92a8239fce4be42ecc2c89f86
+4101ca8dca0a63fcab6a483f3b78d574db2e29198318037022f87c2d2d7c5ae55f006c1e13f69c985a8e14fd45aaa870f9f0
+5a0c0bfc4201d0d61cfe89538b2c162ab2baa5b9af215b4e2d20d70530f6b448f1443e17e0673a29e89d124daced289dcde0
+2749e17f66dcb289b33d4d5136c5e08557e20779324455216a7e6111f4cddd6072cd6e4025fc3bcf2f2eb2b35fae2f1d69f6
+10b0fd34e9d6f81ae61e9b295e6bd65b8335911c7b3b27b1f4e23e6bd9194966f97a7641b9bfe5871f1d01398a1b1497cef5
+1b024577eb100ca91ec50753054ecee718a2d12fdbab98002c6db1f2b3562782147c71bba44bd0d6afe96362a040dbd863d2
+edc34b30187c995d1f95e050c1fe4a21cb96a236c6778bcb156c12e267fc9a2369af1835
