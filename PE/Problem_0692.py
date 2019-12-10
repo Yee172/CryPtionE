db45e6bbc42a1e9cb6d5741d6f3fd9d6e0172be2
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-12-10 14:08:19 +0800

    Create Problem_0692.py

---
+++ PE/Problem_0692.py
@@ -0,0 +1,42 @@
+# Siegbert and Jo take turns playing a game with a heap
+# of N pebbles:
+# 1. Siegbert is the first to take some pebbles. He can
+# take as many pebbles as he wants. (Between 1 and N inclusive.)
+# 2. In each of the following turns the current player
+# must take at least one pebble and at most twice the
+# amount of pebbles taken by the previous player.
+# 3. The player who takes the last pebble wins.
+# Although Siegbert can always win by taking all the pebbles
+# on his first turn, to make the game more interesting
+# he chooses to take the smallest number of pebbles that
+# guarantees he will still win (assuming both Siegbert
+# and Jo play optimally for the rest of the game).
+# Let H(N) be that minimal amount for a heap of N pebbles.
+# Let G(n) be \sum_{k = 1}^{n} H(k).
+# Find G(23416728348467685).
+# ----------------------------------------------------
+# Analysis: brute force
+#           Smallest term in Zeckendorf representation of n.
+#           Hint: http://oeis.org/A139764
+
+6061225bfa6abaeab90078fabe97686eb8612e34060515f924a60049b4df746b8fc2c13d75afb679bfff7e8627afc3a55a6d
+a50746f58b1a901bc92919862487fe1d1266f8b215d62f4a4bbdf66ee1ecd907a5aa0dfd96c5d278ce5fab6e95cdba3bf5aa
+acf908e43d7a61d31389001578cb83ed5cc352b2ce2ec505d05d2f881fe8c5983e223fa0bc7bf3115a68fc4ca1af0815459f
+402223ae30b5dcdc9c2893a0dd1a0aad3576e01dd243628d03ea95ad6f50879c096785ef3c98c0e6a260052d857150ab9be4
+8cd8b7d3ec92b3b965a0e67367d20e19eb1e372b4be206ac58b0350ad4bf914e6fe8773b9d44f8d137e19f4ac68a957758f0
+1c18d2ae544c7c2d2d7ca759f29e11c30df00a09a463ad7c2f96380322a186a66085656c8d0fbfecebd3598af03b3dc5c47e
+b15bd6edcf5bb36eb21d110391b5ccd8491ca7a912e63ab15c9c7e6517833b427e720c7a2d214e4c7b4b8f2766c88aca145f
+b95c3ffcdbc7c5887043f68a4181a182c2ff7adfb31f15c32eb7b7148a2ce0804853fcd9b6aeec89445ac16b9f46bcc5658b
+92bfb5f02e66fcb767fa29318da409f37d932b3ed3c1258cf3491145a0f589b9bb2bdb654c9e1554eb53c814105a7cc4b86d
+d1635325dee4934d562507319027662644d552e0f296f31df731a16ec38d27f6be5d5d8444b27a11c6cf2dea95f6ff72bd00
+a2f9b3a8acd57b362fc3e2aacdf57de97c2d2d7c862bfa27681b1a4da6004452c0e73148751394ef988ce9ac4a6e529f44d6
+683189f56bc71cf97a2bf98660ca79482c5569c228226574bbd85fcfed8fed76bb8bfeb48af445b858c84d25669c560a2d8b
+4f9b663a18fab79d339897b7b32311760b9f55940fde92d6ff39cfc9670ebadb39b0d6c97e83cd26d40e842d8cb7670239e4
+812afa944ba0ff24eedbc11b5142eb32f818baf41896ae30455a3fdc1e86c7e4ba577f77287feea7998aff2917c0fb6f73a6
+11f15108eb20dba61941959c7fe4b466b39f1628693303f5b7017dd4246b90167c2bac8b7ff08be93743d2a678159473eed0
+f5de5b44eb80e612749f1dab38af6666395a56ea4059a0b34fdb7c2d2d7c9d8416125e86863a85ce35d9a5a6e79a5cc7e08d
+d51be99b7ebe27060e5655117f609318b1ab64b2b056c81eca8722caf19d48bab7f94260566d6dda1f6e344e047c1b57c5fe
+af52c5b6b24776054a1077c90908989c061803f384392cefd15295531215773657c894e322dfffbb4ded0fa35e8d16093e54
+a7f56871208d401c7c6a7ec34ba67e8d949e6c6c0ac2cd24d761c3d07a3497a6ea05fe7572a0147b0129e0c179b43a945dd5
+4dbc67ed3de29655528de9a0e159f248f0311e36f8daf01d2af7f0b7c339578f9a528e958d1c5940a3bb102e19b695ec8b53
+a3566e63b83002a5ce60a0cad746e05d8f2c87e9409542ceb5dfecbc539bf94382687010
