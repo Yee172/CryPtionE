380243b67474104f3f16812ddd6d17e58cd6cb2e
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-12-12 22:50:54 +0800

    Create Problem_0512.py

---
+++ PE/Problem_0512.py
@@ -0,0 +1,14 @@
+# Let \phi(n) be Euler's totient function.
+# Let f(n) = (\sum_{i = 1}^{n} = \phi(n^{i})) mod (n + 1).
+# Let g(n) = \sum_{i = 1}^{n} f(i).
+# Find g(5 \times 10^{8}).
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: f(n) = \phi(n) if n is odd else 0
+
+a7cd4c0b10442a6e6b2cfe28f692390de91b9e6cb61f1b562edd54b2209566aeaad310a22fed94253e9cafdd8b9825c147f5
+f5d90a3d414b50d912d708f72547356c190391d24f0a876b7ac8abde24a8bdb3cf407af82b66707583e38302190549c8eff1
+4b014d4eb02cda4bafe66580dd307ed8841de74ba64120be310940637da79a97dbb4bde618ce367dd538e45b87f3b21319e7
+3062c896b0109a2800d955f6b4d7dba1ba98b674a12f33401a0439495bfe364418c66a13e0af06c73d8fd7f3226a88186e15
+04b6da69927068b9272b5a40cc5bdb4d9ac492d879226c709c1505de1e1709afab2b24dc7506af14e51bf7e0f5389bfcc2dc
+33a26f2126e0
