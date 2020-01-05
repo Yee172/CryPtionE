fa3328369f44426cdc7b7f69d872a7e96760ee60
Qihao Ye <11610812@mail.sustc.edu.cn>
2020-01-05 22:23:29 +0800

    Create Problem_0393.py (temp)

---
+++ PE/Problem_0393.py
@@ -0,0 +1,18 @@
+# An n \times n grid of squares contains n^{2} ants,
+# one ant per square.
+# All ants decide to move simultaneously to an adjacent
+# square (usually 4 possibilities, except for ants on
+# the edge of the grid or at the corners).
+# We define f(n) to be the number of ways this can happen
+# without any ants ending on the same square and without
+# any two ants crossing the same edge between two squares.
+# Find f(10).
+# ----------------------------------------------------
+# Analysis: brute force
+
+c9b47f00a17a03e3258d4af56504919c1f4f6785f56ae05c8188ed4c4140ee7e5d77d26c8994b7412124274a8f9b3f72a9c5
+962dd1d5740d2259a2d5e1ad5c38e5ed1855e75512e31df1602c4a49d996698a84b7df7b0789e46c3488428d9e728cb5e45b
+9cc1b1c05efd338247ac1524c835b923ba4388af949fc47f8b066bbdef1d2e20295e0cc3e295f80290bf29128b9b4313e1a8
+88b0c0238bff41ceebfcb413e5bd13e136ef8cb2894a8099a7c67dd98cf0167c6292b52b55f155686e83596d76c52dddc1da
+fc7d0c0e74332e822debe07dd337112b2f88724d6d4550093374ad1c620edbe8a6324b001091adac0c49863b8f3526fcc3b6
+d067f98e0b03
