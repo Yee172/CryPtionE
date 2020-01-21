c11c30f2e1dd500f75a8c49f0a67771f10d1b4ab
Qihao Ye <11610812@mail.sustc.edu.cn>
2020-01-21 22:33:53 +0800

    Create Problem_0174.py

---
+++ PE/Problem_0174.py
@@ -0,0 +1,26 @@
+# We shall define a square lamina to be a square
+# outline with a square "hole" so that the shape
+# possesses vertical and horizontal symmetry.
+# Given eight tiles it is possible to form a lamina in
+# only one way: 3 \times 3 square with a 1 \times 1 hole
+# in the middle. However, using thirty-two tiles it is
+# possible to form two distinct laminae.
+# If t represents the number of tiles used, we shall say
+# that t = 8 is type L(1) and t = 32 is type L(2).
+# Let N(n) be the number of t \leq 1000000 such that t
+# is type L(n); for example, N(15) = 832.
+# What is \sum N(n) for 1 \leq n \leq 10?
+# ----------------------------------------------------
+# Analysis: brute force
+
+933a6b9fc1982e2c7bb500accc409a839adb3bb55d571791a7a5af80e7c9d2584951db28c0cdebce2310f71adee25dce7f66
+be60f0f1d3ef30cdc3a610073eb5bcbc55fc899ca4d133a93aa8bb5debdcc64d81af6202839091a1c35497f889ee195b2c0f
+61fc08277773e688831ffb89f314adf3e6657b7d154b48855b62882677fa2848e9301b887eb602b4ac923dd07674f978d9fc
+dfbc87a589ba2a1b4d31d98df7bcb5fb5f1eaedfb31393c2f0bd787213a7797f43b369bfeae77f5461ff89f7288c9a4bb39b
+193d7aca3ffadbdad8021faee1446012504f5397f083bfdbb4b04794e23c65ef75542d0a90dbe9249dc5f7f4e0f416336b87
+07142ba528f07c2d2d7ca5ef746e195f10600a8f5baaf32a4b125b5d58a3d969144085fff82a44b7ad15800be67943f1eb29
+2fe6f29ee49ee569c66ee6171470eefc87e82a2a4b0923b8e233851107e88f5e169f3028a8366f48638b7e7ffb4491c4f0c1
+e5f0eda39a613d82510dc5d74db33268e7c18d8e4b9305c806448bf26fe5fd9719752e95a6115fd0c34f7cde63eb9be48282
+bc850e1d3ba595bdd4f3c8406db7a4eba0593ba93eadbb3c04f7101b943fe9b72825b97aa96024089226bf5c97f384694450
+b6a774e8ae1a8595213d83d373e6f9d99a1a1e6efbb55203548e326c8f712d2fcdbe3d7d064b01db3d9470a28bc90b42a5fd
+11ec6dba35f6fdd6ef618bbf0e7aaca4
