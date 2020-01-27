8ae6008daf02a1e6b5d57821fb8c509317dc1040
Qihao Ye <11610812@mail.sustc.edu.cn>
2020-01-27 16:22:58 +0800

    Create Problem_0359.py

---
+++ PE/Problem_0359.py
@@ -0,0 +1,39 @@
+# An infinite number of people (numbered 1, 2, 3, etc.)
+# are lined up to get a room at Hilbert's newest infinite
+# hotel. The hotel contains an infinite number of floors
+# (numbered 1, 2, 3, etc.), and each floor contains an
+# infinite number of rooms (numbered 1, 2, 3, etc.).
+# Initially the hotel is empty. Hilbert declares a rule
+# on how the n-th person is assigned a room: person n
+# gets the first vacant room in the lowest numbered floor
+# satisfying either of the following:
+#    the floor is empty
+#    the floor is not empty, and if the latest person taking a room
+#       in that floor is person m, then m + n is a perfect square
+# Eventually, every person in the line gets a room in
+# the hotel.
+# Define P(f, r) to be n if person n occupies room r in
+# floor f, and 0 if no person occupies the room.
+# Find the sum of all P(f, r) for all positive f and r
+# such that f \times r = 71328803586048 and give the last
+# 8 digits as your answer.
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: list P(f, r) for each f, then find the pattern (guess the conclusion) for each f
+
+32a07cab1967f5f6c3acbe4cf2e3ec0384f20073ca5fb58541b41c471b734c0917b5c12c9c498dde623d63b904cb278f8576
+edafd5dfe1955a5189f5b0b465d7b7e0c33f42c096b9bed8f5cb18850588a6f36ec68573b7b6b5d65e2b310ccce28ce033ee
+05a890ad543a95080e3e0fb4e0ea70c47bc56a624ac52bf206918e3836ced6aad3012d31ce539d216d5f08e94d6b4f4108ed
+0526cb9461b9d746c42d2c386a56ade3ff45d3a9584a63a260703baf3629201ebc63ad8f5db40f0d3f2f4f2b0c79f7eb9c96
+c97d7fef090a8a8e915dc659f9b6eb36ddfdba3244efe29e17879979017f2501670e682372c2516ab4e3d3f0f1a8ede6aa84
+c8b5e7d4967d7c2d2d7c91e530d3cc953fb019bd677fc6d008785d9a97b734681c1fe1b6571919eef0d453617d0d2f10ca2c
+1bb29e67d2c456de510698bbefb97b6c980e433bfef0b099608dff205b582ee37dd4f000aead4382418e043f41d2f8921fbb
+a4078aba64213f1b18f0838de7173f8012e489405c217565e4336f4fd9945cfa9d66c8ddb915a97ba670a4e2308d9153b109
+25a96dacffe002b4edd52120d7c73451185431edf46762dc36a89831e626d1247007cb3caf6f93a2499b472b2581bf80efce
+2e9ae2fa9acf0183e5028c569cfce14e22209f5ac5f863ae64286938450c2cf7c6722b17b11c0e04fd570c6bf1d234521e88
+3901161effc2f890742cd26221b606427c2d2d7c2fa0257830b36279dbcee11ce07d66bed30774bae0040c6061ca2725cf24
+9283a344a8fc8ce8fa63d26af7a8ee2541940b0577273020005ae1e65e86470d3915a722e8c06a1b9829f9c4a22bd15c014e
+fd06f76e9756150aa8456faa0449405f397b3e83f4faa39f69deca037c785a0203f65b26640000c71fc6f0e903bb188d0116
+21a99cc6033e8b3ab05a66d9ccec4aef0ec3c7ef85d7281c291f08797e4b0f81a60435b67510d332045cd578137b74e4ea57
+cfd9356e7b97f00a27c101c84d062777b1c2c1dd014a987e77f24df97d0ef9de2140593c3947ffa2db5bdb35133f33cb3676
+eb9727d59a2c54fb926ac8588de32d0b905045990aad0ffbbad1
