621408a308809ac8347d540655d985f8bada2144
Qihao Ye <11610812@mail.sustc.edu.cn>
2020-01-30 22:29:41 +0800

    Create Problem_0523.py (temp)

---
+++ PE/Problem_0523.py
@@ -0,0 +1,23 @@
+# Consider the following algorithm for sorting a list:
+#    1. Starting from the beginning of the list, check
+#       each pair of adjacent elements in turn.
+#    2. If the elements are out of order:
+#       a. Move the smallest element of the pair at the
+#          beginning of the list.
+#       b. Restart the process from step 1.
+#    3. If all pairs are in order, stop.
+# Let F(L) be the number of times step 2a is executed
+# to sort list L.
+# Let E(n) be the expected value of F(P) over all permutations
+# P of the integers {1, 2, ..., n}.
+# Find E(30). Give your answer rounded to two digits after
+# the decimal point.
+# ----------------------------------------------------
+# Analysis: brute force
+
+3e303f090091fa35bc8e080a3f254c90c02958d0927f85280f03c0a7dcd705b839360d025e9f708a7eb99d1654fbf688b7e9
+89d06fab38bdc939c5d8ff8021209dc7e5409f7c31b0f1ea7568027c42240b6f8412136103ef45848a189ef176ae0c1f7569
+f0398e14aea1839f219336f84b55e794e1f816fc9133f7a41e21c2a4f40547c2c4409a130868474e6c06dce7f4663168fc53
+48b11d79bb0e7f8166610a613e21b408c57faca4c0e1e27ab60c29abd22b025c216bf1d73d7e0e27653991943ee3b8fe8725
+c36c090f4db5d8b36e415207b21886ddd4efb1f603fe22eb0f7d71d327e849430855917832cc17b990e8590a4f14fc3e65df
+fabf79d60232
