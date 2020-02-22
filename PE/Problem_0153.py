380deb00cef063d703d3a47e4433fc5bc1411f67
Qihao Ye <11610812@mail.sustc.edu.cn>
2020-02-22 14:44:43 +0800

    Create Problem_0153.py

---
+++ PE/Problem_0153.py
@@ -0,0 +1,44 @@
+# As we all know the equation x^{2} = -1 has no solutions
+# for real x.
+# If we however introduce the imaginary number i this
+# equation has two solutions: x = i and x = -i.
+# If we go a step further the equation (x - 3)^{2} = -4
+# has two complex solutions: x = 3 + 2 i and x = 3 - 2 i.
+# x = 3 + 2 i and x = 3 - 2 i are called each others'
+# complex conjugate.
+# Numbers of the form a + b i are called complex numbers.
+# In general a + b i and a − b i are each other's complex
+# conjugate.
+# A Gaussian Integer is a complex number a + b i such that
+# both a and b are integers.
+# The regular integers are also Gaussian integers (with b = 0).
+# To distinguish them from Gaussian integers with b \neq 0
+# we call such integers "rational integers."
+# A Gaussian integer is called a divisor of a rational
+# integer n if the result is also a Gaussian integer.
+# Note also that if the Gaussian Integer (a + b i) is a
+# divisor of a rational integer n, then its complex conjugate
+# (a − b i) is also a divisor of n.
+# In fact, 5 has six divisors such that the real part is
+# positive: {1, 1 + 2 i, 1 − 2 i, 2 + i, 2 − i, 5}.
+# What is \sum_{n = 1}^{10^{8}} s(n)?
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: when gcd(a, b) == 1, a Gaussian integer a + b i is a divisor of integer n should satisfy a^{2} + b^{2} | n
+
+af1ffd986aab27857b9f5eea569d128e0687138c10e5e137211feae52d70d874e7632deb4ec993b34b3c3fbacaa9c7c64a9d
+5c888bf7b3fe0dad2e23e33bbc843ea8be4de092ac9a15b7d251991d617d2bf26c345be35e02d98553cfb1fb410b9ff87b3a
+2a21173ce70760decb05ad17672e23a68385ef57c03f7bbd2b8abeb92a0fd097e608ea612c33b333053e49714ce6aea36191
+e0cef86d96af204702179d156b7c725dc907bedabbb64e95272117b2f3e9c1410f24e44c692f66608a881514c794fc9124aa
+3ef01a39a8f294f9eaf2edd739ea6884ea5f3e9a126a196698d38a28539dd23a83e2633aa097b3be7ad2d7c203e5bcb00bbb
+1892726495c67c2d2d7c8ed8da973517ecbcab570308763f2564d9dee636f7caafa37951c91d96afde8b2578709eb85a7ccd
+8fa1dd2953ef00b878cd8dd142e84064c799dc8671c30898137a089ffe46262b340aa1ceebaa54c3b845aae29fb17de71d2c
+6042cd2741f1b145c66852eca4969d4e86ca1db04de27feb4d33ce21fc6931327d791bb163ccd5018f0738499dc26f0dae93
+1de5506407f27c45ae2286dfde929af37c262df0a4773ea6ccfc1ddc56101ba44e387ec48de97fcff28abaaae6e7c8f79930
+a241487a9d3fa3909cf93227f4be1f5a4ed7b895c6fbe1c621d16b6c2763abd929fb2c6626fa12da4fbfa4d3c353bd0d40cd
+5f5d584ca4dac2a2673000d7ea52c6cc7c2d2d7c5229357e27a68c8c3f75a78a50c7531d5cf7702da08a7e8e7c09797a6b8c
+6e561a9e4c5a42da03b621822670999e46a559fd789452795ad0f725f1a20ddbff5a15b45fad90b91d0a6c6cd25ef7117dcb
+d56bc852cdfb947921f90978d433494cdc84a6ab2b804104da3c4c6a213732ae8eaa319cdc8c19b932382d76c673b0b9cd22
+1cfc59afa56622f10318dde02846dca409af06ea39759e78827758d303648963e358cc33b57ebda677c47badb93236de4101
+268b17ff1c288ed261da90ecd84cf523d8b5a931761f7646439d4dfbe99d5f9e923aac748cd56e16ad924247a6bd6f9478b9
+f88619806bfa38b2399931c5d00ee7db51a14220ac62368b0e7a
