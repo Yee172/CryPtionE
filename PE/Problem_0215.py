89d27e7fbdc16b738960493df90aadafdca9a689
Qihao Ye <11610812@mail.sustc.edu.cn>
2020-02-26 18:58:37 +0800

    Create Problem_0215.py

---
+++ PE/Problem_0215.py
@@ -0,0 +1,38 @@
+# Consider the problem of building a wall out of 2 \times 1
+# and 3 \times 1 bricks (horizontal \times vertical dimensions)
+# such that, for extra strength, the gaps between horizontally-adjacent
+# bricks never line up in consecutive layers, i.e. never
+# form a "running crack".
+# For example, the following 9 \times 3 wall is not acceptable
+# due to the running crack shown in "/" and "\":
+#    |        |     |     /     |
+#    |     |     |        \     |
+#    |        |        |        |
+# There are eight ways of forming a crack-free 9 \times 3
+# wall, written W(9, 3) = 8.
+# Calculate W(32, 10).
+# ----------------------------------------------------
+# Analysis: dynamic programming
+#           Hint: if the second parameter (in this case is 10) is too large, consider to use matrix multiplication
+
+53598283830086094b742b06444f2eacdd9f090f9982485f3f34bac3a6419ecd024bb468fb140f4e08b4026062dfd1f9a6f6
+d2af9adb5d387a336450f1eab756c2832ad18427118ee8ee7b2193dca9835b7c8e65fb79c19f64c0a153d54b20a53cbef186
+5d294101ebeecf3f63781fa6d45dbd7f4794d5fccb2fe785c4d8bd386047086632334acd0b67a1796cc7a541a242ec50e377
+c71d968ec97337845ef55cbe5d1c8b4ba9aa031cbad8efa2a0f8d69898c1b15dd2b9230f483cc5163c8e51f8ec24b01e2016
+d1cf3726bd17c9c4a1a9ec80d070b8dc1940200da030abdcd84fbc79ac7d84ac40cd24868054be2c3f04d5e8e7df351782d0
+fa4d521fa36e7c2d2d7c12cbe50222b91e7728fc532632fb8b82ca243fe5fad3ab34f9e38d86ddd89500c76743c0cc9bee89
+dabadc9cef91b69cef09368567ef9560560de17a50918cb0b871aa8b92aeaa3ef625419921d75e4587b5546eb179ffaf36fe
+dcec3fde027dcbf5ac166939d25e57d50e6a712a2adb4baac151a1f092274a748bfc884e010da1ace10d2a4d06701be31e7b
+05c50ab4acb46920d083facbe566885d7a48b29421e692768093ac8ccf6a2047f9512d8f6f6dd612518b92bae8e6f863cfac
+10a83fff1683907cc86677fa8b8e1a973444d7a5195ba492a629bbfacb9546a09a32de3ce305efef53872972d0da859d3bec
+6e2a0c7b58a99f9dd169c4bf9b6938287c2d2d7c52af24890c843ddb328da9fa0e59fed7c0c9622c4b9b5f21a849e8453b63
+5306f490b118ff6ff90bc18f990eafd0a70b28587ca56c30a5425c226daf458e9ce21d01a35bbe95d6143455ce56d3eff7c2
+72dc19ceaa5bd0ef0490f511b6671cb6b7d8c6cb4e7566ea474c5de5d0603eda8794faf34771b4192efb5cc75588050d4de9
+218ef1f7565618cf2c2e9887bcc17efd8d2ee426a6427b6c2d945a06e07985ac3e06610cbc27d69e29a7682681ddb45cb873
+e169bcb6fffa45c5b64e0f71744e534b1cc66783cafdd600c9c3a8af3564965a50547bba5ac0d741b71c7b952f1d8e0d6349
+5ba35ba8d9d6dbcca4cdc290e8d2a4c20e2fffd2f0fc978502737c2d2d7ca0ce03c279b467412cdb91b4783cf06444899cf0
+af8392361831492d2e9e55e12d7534a0938ae74d2d2fb572b31c63ee5757eb7d1b2d107f30c244aa38f397a3480763ac83d2
+2b8bac38c07397ed683a47474684520f31872286d97c42ab2c55f8f2f20ceed3a49583351313ff56da0d04283028dc44df22
+8d10e9234567434e712ba7054dd2313e16c9e67b14d11e2041dde72bd3c685a98f69e8da444550026ea3df0ece105ff044bd
+5a6eb8eb32721b0d1a81682fb9da64120cca229b56db37b70c82170543a0dcf88e779a3e817aa4f493abb47060ecb5d4c3ab
+ea2d84d444fc9d775267d758e8d90a4e9f3b42e89ed9f801ee46366d3753e33371ec764f
