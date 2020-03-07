79917e97bd17033848755e9915ffda1c39a7c47b
Qihao Ye <11610812@mail.sustc.edu.cn>
2020-03-07 22:02:19 +0800

    Create Problem_0313.py (temp)

---
+++ PE/Problem_0313.py
@@ -0,0 +1,23 @@
+# In a sliding game a counter may slide horizontally or
+# vertically into an empty space. The objective of the
+# game is to move the red counter from the top left corner
+# of a grid to the bottom right corner; the space always
+# starts in the bottom right corner.
+# Let S(m, n) represent the minimum number of moves to complete
+# the game on an m by n grid. For example, it can be verified
+# that S(5, 4) = 25.
+# How many grids does S(m, n) = p^{2}, where p < 10^{6} is prime?
+# ----------------------------------------------------
+# Analysis: brute force
+
+834feeb3ac9280ec95a70295cf58bf7d2988a7d4830e88bb9dc3ee6a98dee29e32b4a24ad07923512abb3201e0343fa3d5a1
+66e66c4245094979b71a41a749bea8033dcf0c3edb4ee61f2ec09814b3d4b90e203041bd3e276f9719ffcff149b9fd1ae644
+b544a33bb3e67316f87a53e99a0316518648d48aa47a076632eebf0dea3000fa4a6a95027322855dddca59f8736a677b6b74
+89e3eefdd488269b3d293fb972d0c174d4de15493462d72bc73274238b84cf2ab9bd6a1d280119a789b66cc3acb9bff71dac
+e5d29e8c8f6a29f1774b6b50d437819a8a241ea8eabd18464f05d44902a132dfeab06c87a2819ced40cd6ce3f5fba2a0cfb5
+f39d29e63d777c2d2d7c585c8175d9c5cf176d0dc3a43e0f1d70aba8c99d0cc06fb2b87540e94602b0ff881296b9d23885b0
+d1363381aa8aa8640752e1f36c065bf2ffa8206a1c34e6630e77cffd54e3208d0dc8cf167700e870ad48fcc1a69a2e691420
+c82b435840401944fff3e5115f456112a0aaeebab12463070e90d02a09dee9781da32e55c765d5a8aa810ba97e7632dffdae
+54fa4296645ff8b0dc409fb62d6843be5a0d8fd3456f5cf7b125ff2916fee60e9752fb6a53e23a49293fa033d513fbc5c2d2
+236d3de3f0db16da8dabf27c8bd07177a48193868b885ea22b1f3c74fb484b8e501e688069ee36d24eb6695a9484e5324a13
+b5f187330c2c8dfc3d3d0fbe523457ad
