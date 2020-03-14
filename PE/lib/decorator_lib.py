274657f4c784183a695703ed52d61645fc18c64f
Qihao Ye <11610812@mail.sustc.edu.cn>
2020-03-14 14:26:19 +0800

    Update decorator_lib.py

--- PE/lib/decorator_lib.py
+++ PE/lib/decorator_lib.py
@@ -5,8 +5,29 @@ This library is for storing decorators
 
 from functools import wraps
 
+__all__ = ['memorize', 'track']
+
 
 def memorize(function):
+    """memory wrapper
+    
+    Set up a memorized function
+    Notice that this operation only memorizes the args, kwargs would be ignored at memory
+
+    Usage:
+        @memorize
+        def f(...):
+            pass
+    
+    Decorators:
+        wraps
+    
+    Arguments:
+        function {function} -- function needs to be memorized
+    
+    Returns:
+        function -- memorized function
+    """
     __memory__ = dict()
     @wraps(function)
     def wrapper(*args, **kwargs):
@@ -16,6 +37,7 @@ def memorize(function):
             result = function(*args, **kwargs)
             __memory__[args] = result
             return result
+    wrapper.__name__ = 'memorized_' + function.__name__
     return wrapper
 
 
@@ -27,6 +49,31 @@ def __track_print(function_name, string_parameters, __space__=0, header='', tail
 
 
 def track(*t_args, **t_kwargs):
+    """tracking wrapper
+    
+    Track a function, usually a recursive one
+
+    Usage:
+        # default tracker
+        @track
+        def f(...):
+            pass
+
+        # tracker with user-defined indent
+        @track(4)
+        def f(...):
+            pass
+    
+    Decorators:
+        wraps
+    
+    Arguments:
+        *t_args {[type]} -- [description]
+        **t_kwargs {[type]} -- [description]
+    
+    Returns:
+        function -- tracked function or tracking wrapper
+    """
     __ratio__ = 4
     __get_function__ = False
     if hasattr(t_args[0], '__call__'):
@@ -36,6 +83,19 @@ def track(*t_args, **t_kwargs):
         __ratio__ = t_args[0]
         assert(__ratio__ > -1)
     def __track__(function):
+        """tracking wrapper
+        
+        Track a function, usually a recursive one
+        
+        Decorators:
+            wraps
+        
+        Arguments:
+            function {function} -- function needs to be tracked
+        
+        Returns:
+            function -- tracked function
+        """
         __space__ = [0]
         @wraps(function)
         def wrapper(*args, **kwargs):
@@ -51,6 +111,7 @@ def track(*t_args, **t_kwargs):
             __track_print(function.__name__, string_parameters, __space__[0] * __ratio__, tail='-> %r' % result)
             __track_print(function.__name__, string_parameters, __space__[0] * __ratio__, header='Leaving')
             return result
+        wrapper.__name__ = 'tracked_' + function.__name__
         return wrapper
     if __get_function__:
         return __track__(function)
