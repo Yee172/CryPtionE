8499f4c3ca8e11888e728acd3dcfd219229835da
Qihao Ye <11610812@mail.sustc.edu.cn>
2020-04-03 14:09:45 +0800

    Create Problem_0163.py

---
+++ PE/Problem_0163.py
@@ -0,0 +1,52 @@
+# Consider an equilateral triangle in which straight lines
+# are drawn from each vertex to the middle of the opposite
+# side, such as in the size 1 triangle in the sketch below.     
+#                o                            o
+#               /|\                          /|\
+#              / | \                        / | \
+#             /  |  \                      o. | .o
+#            /   |   \                    /  'o'  \
+#           /    |    \                  /.-' | '-.\
+#          o     |     o                o-----o-----o
+#         /  '-. | .-'  \              /|\'-. | .-'/|\
+#        /       o       \            / | \  .o.  / | \
+#       /    .-' | '-.    \          o. | .o' | 'o. | .o
+#      /  .-'    |    '-.  \        /  'o'  \ | /  'o'  \
+#     /.-'       |       '-.\      /.-' | '-.\|/.-' | '-.\
+#    o-----------o-----------o    o-----o-----o-----o-----o
+#             size 1                       size 2
+# Sixteen triangles of either different shape or size or
+# orientation or location can now be observed in that triangle.
+# Using size 1 triangles as building blocks, larger triangles
+# can be formed, such as the size 2 triangle in the above
+# sketch. One-hundred and four triangles of either different
+# shape or size or orientation or location can now be observed
+# in that size 2 triangle.
+# It can be observed that the size 2 triangle contains 4
+# size 1 triangle building blocks. A size 3 triangle would
+# contain 9 size 1 triangle building blocks and a size n
+# triangle would thus contain n^{2} size 1 triangle building
+# blocks.
+# If we denote T(n) as the number of triangles present in
+# a triangle of size n, then T(1) = 16 and T(2) = 104.
+# Find T(36).
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: https://www.math.uni-bielefeld.de/~sillke/SEQUENCES/grid-triangles
+
+27a47a7caa2e2e8422b5ff40b7507ab2ea747e6424fdb80d059f6e1fa376614c9a4eb0c3c52374d6739b57faeeae14c69f7d
+b7d99fd5e5fa72d010ce7a4a737e2ddcd42975ef2e8b1f63c6a274dd12382e2ed13deee953e281cd65fe6c2d99451e4aa470
+d666ee905dfd770f6a931444356949e4c98afb74e2d6566af39dbba965f3b7403d1f08354619ff1d151be8765d2fd0acf2f2
+55a1f359513c8a6156e4b60881494fc198365104652d79adc41517f2791f673608b66cbe49c4d33484284080201e8c5a41c9
+9e374febdf357d37b62384186ecf8a2b080c203eaea75cfe6dd97874b76b304ada01a50a2a33936d3865cd582d64d36d03c6
+2e588ddcccad7c2d2d7c62d843ee784bda910c09e7bb6a2274b71e5bfec091bb3cc44c88d3c3356b3216b981477a3695eed7
+989125e71467e6e077f143ad928ccc50a7d9e28b45e6744a0e007f42a8c9f9f3ed947ae436e651fd29f0aa66600b9f753dcc
+4438ca2242d010c6c4d4c8518b3882bfafb46db9328910ba24000d38044b1b8b34c25489b616802388771695300c9d9926aa
+d1b2adee016b9ecd0f6f5550179a88913e7d50cb3a0a14ea485a3c88c9e9670cef3d53a173628e4c1f3f099912cf0b5444d3
+59b5c0925c38d01ab9cca3cb73acd29d33c255f12449b81b15f1ea91db960152f4776e78b30e6d42956417517f8805b4871e
+7a1e4c9c198a754f9a14e4c368a320f77c2d2d7cb859ca14ef776253ead7a6b93d2af5b4b2460ae19ce5aaa6c8abb1df083e
+63354e20c8a8278a6d299da32490e7828504a97fc684933add9d576af42a82e4197a9879597ca28f2bf22ad4ee91b4a5751f
+bbcda3ddffcf312526cda6281e68d19682a84ddd92b500c720d431adcb16e0497f606b034ac660e6afa08f0dbb3e3cdb358f
+ba5b42641eb60ce2eb12b40b43a649fd9319f1210ac65f8161f7ee3951b18bd47ac3d1bab78b1605e05542e48ee2334c7b72
+aeba2ec28e68a6c3e27a16c3eee8eff7029598d3504a28e1754036ab4e7405677348845668e26783bbc72bce661ba88fd23d
+cd892b6f353bb7939ce27297a3099cbdf1b75fd6f4df733c56eb
