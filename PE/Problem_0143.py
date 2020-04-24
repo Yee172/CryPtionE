194eb915d5ba249fa6ab7df029ccd165a054171e
Qihao Ye <11610812@mail.sustc.edu.cn>
2020-04-24 20:32:40 +0800

    Create Problem_0143.py

---
+++ PE/Problem_0143.py
@@ -0,0 +1,47 @@
+# Let ABC be a triangle with all interior angles being
+# less than 120 degrees. Let X be any point inside the
+# triangle and let XA = p, XC = q, and XB = r.
+# Fermat challenged Torricelli to find the position of
+# X such that p + q + r was minimised.
+# Torricelli was able to prove that if equilateral triangles
+# AOB, BNC and AMC are constructed on each side of triangle
+# ABC, the circumscribed circles of AOB, BNC, and AMC will
+# intersect at a single point, T, inside the triangle.
+# Moreover he proved that T, called the Torricelli / Fermat
+# point, minimises p + q + r. Even more remarkable, it can
+# be shown that when the sum is minimised, AN = BM = CO =
+# p + q + r and that AN, BM and CO also intersect at T.
+# If the sum is minimised and a, b, c, p, q and r are all
+# positive integers we shall call triangle ABC a Torricelli
+# triangle. For example, a = 399, b = 455, c = 511 is an
+# example of a Torricelli triangle, with p + q + r = 784.
+# Find the sum of all distinct values of p + q + r \leq 120000
+# for Torricelli triangles.
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: 120 degree -> b^{2} = p^{2} + q^{2} + p q
+#                 ...
+#                 set a graph, then find all (p, q, r) pairs from (p, q, b) pairs
+#                 http://www.geocities.ws/fredlb37/node9.html
+
+a60469b15dfcf907efa3e4a9edc2cc0afd59da6a89187abebd5fa77cfdfd6cf686aa5da6c03f564eb6bbdb2a28cbb08c5f71
+5cec96eb319a8ce8c835f087129528a5efda28ed87a59ac42679b2be78614eb336fc284e3a23afba3262f0696461eee1b036
+c41d0d6325b61a9fc263edabe2d574b589e94b8c21cdeacd7221fd6891cd31c56a5cc63320368b28cbba2ecf1992dc730c2c
+af3ab5d281e069ac129d4ea65fa601cdfd96daafd1542e7d29b2e194162e685da8d84eae3ed918fd8b10a93c10e17ee3c832
+14f9dcb1d816589f50f245b47e03612f9a52e8f3aebd246fb10d87f0aa3ff3c831d211da2b8917a95a9052b2814344bbbc4f
+8e03e3cf2e747c2d2d7c856d1d0b5c785689c3b6f545f061c8b959c4dd2a20edee0a188eb6440a690de51a23dee5dfbf5fec
+989057c651011ac1446d594780ceaa14541c63199209948a447624e582b197717e8ff357c020cd6f4f923977df06d033bd84
+a74b678e4a5f914f0c48b1151b04b3c8e5fca7f73220cb80a462f4bd00ffa1bfa5544bbdb823029c4b7282425c3f2e22ef95
+1593c54f58ea2ed22408650d81fdaf4105b52e60e4abfc9521088d986bca9a38440be09c366a48e3f405597ea70ea64689db
+c624fcd396828dd06b26903ec031f4eb2c5cb93172e9777d1114f773e6d17c5da1876ad9af4651b60fbe5d49dbb52b94942d
+a0d6bb02f885c70689f402c05ab8af1a7c2d2d7cb020f387e2d3e14de776d89e5c6d0638ee38a7aa2f883690e28c390f63c1
+693d658d572beb89bb3479e0439ab4d9637e44e7b6a8bffb2135b3c558786effae05a737a1f02fd8fe01f34357a2e1937771
+36f620b965bd6347ebe47cba65cda89d7945c2cee9e7f78434dbdc7ffaa52b27cc05d11ef792710fdcdcce1602633b1f3bda
+d01f6487f97645c1661e108d0b3bd355b25b1f08eded8a8d106e4fd302e92d4a4b7cd58054df5c779a0247127c018a504257
+3f646c94f7b0ecbb50af503de09f7fcd44ad2dbe2b7cb278efe33f0a9e79905efa8f3e5e9724f3c4f17cc1e8b8d3585f75e6
+59810bc7a778db8e559f9d05b647c61bfcc7cbf7c2fdaa299d937c2d2d7c4fab76bcc0d9779f3baf3ac1447905de009aa436
+0d46b401763ddb8e3922bd5a6c64280747924caa73e4b032fb07eecffc9c086515722d674729624850515dca888002d34c75
+a2f0c188584c27c0700c4e49bfad4f9fb71dbeb7cb865201346c4d0b2b62806c6ac4fce999406b245ad757f1227f18b3cabf
+a2917d8d579b986f5749fcd961624152edb30188f6fcc1e4e446dec8ad8efd0077b5cf9c2e5affd7126b53b6f3badaf69262
+671657797c9c7fcb33a1f6b19d60e812cba3ef07b318571541c77765de49e13c5891af7832fafe95227f2839fddfbb01e809
+ad9bc7c4c0250a20c44518c03e0f161f501ba9da503d686bf9da96b06a997f85fa4b6468
