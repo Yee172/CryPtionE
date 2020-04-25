9053e86396c40c2b64f6baec4a12884c52d80955
Qihao Ye <11610812@mail.sustc.edu.cn>
2020-04-25 19:46:37 +0800

    Create Problem_0155.py

---
+++ PE/Problem_0155.py
@@ -0,0 +1,32 @@
+# An electric circuit uses exclusively identical capacitors
+# of the same value C. 
+# The capacitors can be connected in series or in parallel
+# to form sub-units, which can then be connected in series
+# or in parallel with other capacitors or other sub-units
+# to form larger sub-units, and so on up to a final circuit.
+# Using this simple procedure and up to n identical capacitors,
+# we can make circuits having a range of different total capacitances.
+# If we denote by D(n) the number of distinct total capacitance
+# values we can obtain when using up to n equal-valued
+# capacitors and the simple procedure described above,
+# we have: D(1) = 1, D(2) = 3, D(3) = 7, ...
+# Find D(18).
+# Reminder: When connecting capacitors C1, C2 etc in parallel,
+# the total capacitance is C_{T} = C_{1} + C_{2} + ...,
+# whereas when connecting them in series, the overall
+# capacitance is given by:
+#    \frac{1}{C_{T}} = \frac{1}{C_{1}} + \frac{1}{C_{2}} + ...
+# ----------------------------------------------------
+# Analysis: dynamic programming
+
+ab7de373b7abd1d01b5b1ba107a03fb203e41bf8668c20a4e642da090a1090dd54aad263ed3de067a5f0a076976b0077a395
+6ee5c428a0df91c45cb0457aa6b013a585a6ab0f772fbbf8388caf55e8ee32cbc380328ad8c4308b9e3ad57451c737ae509b
+ea0ec344767c95b79d44079d535c98ca5272fbf1f323689bbc92ccd946cd20794ee95aa9a2a6739b2a8eeb5015ede659dd1a
+f186239cb7624d15c16489aa531c0142f1f949e96d063f201711fff2cfdc6d9dd04fc87694fea978baccbf905459868e858f
+a3f09a8efc3c383f8cad03c4cb1c99389d3b2d01963db94bc3bca1fa9af8ec538d6ab1df6db97fc7f6140ab9241dffd01d52
+c15f9dced8e17c2d2d7c981c1deff40be5c7015b8052eecf3b9ffbd9521fd1edb7bad596abd0b9487589e32ce987b3217568
+acf24f8435cc81ede0da0d13837a5910bc26931a2bc7d1a801bdffd0a635f146db0c5bdf24680887f28e807b11dc8e741040
+01ef7bf0a10f4beb110734abcdb08fa3fe8aeb25bd4f1cd7d7cfda71371dd17f6f55249ba798e170374557d778777e54c1e8
+dd15bb010a0a1876fab4d44f08da0f2b224abc8bd5834926e78ed2bf90274d0f50ac6b40b2937f2d985cac8849310a008e3f
+36919c10833052a7fd5b68c7cb9348d994858386427bd46c5d9744d0a6b43563642507063772f6c61959f10f6079dd5e9543
+8af5b0065e109e9cbfb5396f30620c26
