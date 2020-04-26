9fd84ac54a9e564bc6c0aa04ef8865a86ad6c625
Qihao Ye <11610812@mail.sustc.edu.cn>
2020-04-26 22:14:34 +0800

    Create Problem_0713.py

---
+++ PE/Problem_0713.py
@@ -0,0 +1,31 @@
+# Turan has the electrical water heating system outside
+# his house in a shed. The electrical system uses two
+# fuses in series, one in the house and one in the shed.
+# (Nowadays old fashioned fuses are often replaced with
+# reusable mini circuit breakers, but Turan's system still
+# uses old fashioned fuses.) For the heating system to
+# work both fuses must work.
+# Turan has N fuses. He knows that m of them are working
+# and the rest are blown. However, he doesn't know which
+# ones are blown. So he tries different combinations until
+# the heating system turns on.
+# We denote by T(N, m) the smallest number of tries required
+# to ensure the heating system turns on.
+# Let L(N) be the sum of all T(N, m) for 2 \leq m \leq N.
+# Find L(10^{7}).
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: https://en.wikipedia.org/wiki/Turán_graph
+#                 https://oeis.org/A078567
+
+5a0769f9338eb10393ccbd20d16a18627e1805063707fa8e0ae025a95cfe18c44c38856915f71b14dc89b1f5196ded54b8b2
+df67e3f0648d42cbeb4e37dc6327ef4f2d9391d3708fb6d6effea9e26610487dea1826212bdf2021494db4a93d1d598b05a9
+86bf9c32545a35333a64a5223c6a5a929e7367ecba2c96c89986878a44285fdf99b37e73efa076ee650b622d449809feaf5b
+780b133627877abe0ede7397fcb16b06c368e32f9fc52cad84b2a322371076e4f737b46df3fe05b7833690a1d180b7c8604c
+819b8cd99c1fc72557ccc8d9416e2f3d0f91491be1c1176383caa4e05b15567ca54bd5dae73e2cd71fa126b705e5bb78d547
+f9ed47fa4c6f7c2d2d7c17a2e36cac8ac3d37fe07cfdbf7f5d76dca24260df6b2f6a10f614e3ad236b5cceaee2ff3e631805
+8d77312bee4b303faa7c0c76fcf56be3643ab2cc07240dfd1021730e06f1a047cf3e6ef3c2097d2a65f8b0e3fdd1736c3d52
+303fe783ed5becbb471e2df6c9764c992977cf9bcd034c1244e2f2097afccdf9419a3121fd68ef5a27df999a788f8755e783
+808f67f414367bf822792f92ae9ed4658172d66851f95d72a602c0b687c1a929ad3288b500a329c52bd17bc4f0159b4049ff
+ca0f9b7eee515addbd9ad1a2474edd5bb94802a0424919fa12f214bd3645d051687bb8355bc9175a5d52f88d0cce6194f73f
+1ff2b3bafce811c03471ef659014ebf7
