c52f1d1c608f93e9f6939ade454efef2727af18d
Qihao Ye <11610812@mail.sustc.edu.cn>
2023-04-06 15:39:37 -0700

    Create Problem_0836.py

---
+++ PE/Problem_0836.py
@@ -0,0 +1,19 @@
+# Let A be an **affine plane** over a **radically integral
+# local field** F with residual characteristic p.
+# We consider an **open oriented line section** U of A
+# with normalized Haar measure m.
+# Define f(m, p) as the maximal possible discriminant of
+# the **jacobian** associated to the **orthogonal kernel
+# embedding** of U into A.
+# Find f(20230401, 57). Give as your answer the concatenation
+# of the first letters of each bolded word.
+# ----------------------------------------------------
+# Analysis: brute force
+#           Hint: 20230401
+
+5187557a5b6801981bd996735d5ed7c733207c5226fbf028748f850becdf962a6906e86b66303be3bfa1807637b31fbad9eb
+311d9a683ca9393ece6067aeb3b6da72eef2b5b6123c4250d50cc590578a5c99c6667dda7075452fdcfcd1974a86dccdff46
+2f93c5581dc24cc5f20268d305aa421842bdbd3884f95a1cac162335e9a0d325b9c7f96475c555272c5c61eabfdd5cd67875
+ad2049d4e0c164b8f6e5f3209dccc06a1d6a094b27a3bb458fc6ec4a242b70c09a48a599aeac718d9117c7a5574fb63dc823
+83ebbd9d4ada3ba3a5ecd33beacc6f6c9778c6fd007c0c80139948d19cf2c5488f8fcf5ecd6dd167b49e9b629aa915137801
+7e0daf7b85fb
