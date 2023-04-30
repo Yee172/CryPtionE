e4f1c459b3292a2e76c175a729294653fb93bcd0
Qihao Ye <11610812@mail.sustc.edu.cn>
2023-04-30 12:53:58 -0700

    Create Problem_0841.py

---
+++ PE/Problem_0841.py
@@ -0,0 +1,39 @@
+# The regular star polygon {p / q}, for coprime integers
+# p, q with p > 2 q > 0, is a polygon formed from p edges
+# of equal length and equal internal angles, such that
+# tracing the complete polygon wraps q times around the
+# centre.
+# The edges of a regular star polygon intersect one another,
+# dividing the interior into several regions. Define the
+# alternating shading of a regular star polygon to be a
+# selection of such regions to shade, such that every piece
+# of every edge has a shaded region on one side and an
+# unshaded region on the other, with the exterior of the
+# polygon unshaded.
+# Let A(p, q) be the area of the alternating shading of
+# {p / q}, assuming that its inradius is 1. (The inradius
+# of a regular polygon, star or otherwise, is the distance
+# from its centre to the midpoint of any of its edges.)
+# Find \sum_{n = 3}^{34} A(F_{n + 1}, F_{n - 1}), where
+# F_{j} is the Fibonacci sequence with F_{1} = F_{2} = 1.
+# Give your answer rounded to 10 digits after the decimal
+# point.
+# ----------------------------------------------------
+# Analysis: brute force
+
+049997ee58c73b9f81e8183006261c41fe7a281f6ee3795feb14df61c9d039927cf2a0aee4265d10b30caa1fcbe159de9114
+a80a49108c786ed17218ecf0a5d9931e82000ec6b7b8c47bf8aaa022673c62b9e9c4d210d0d08dd4fadcba94affdf3bedeb3
+483329c9559454ae9876d93df6b7fc284d1faf442bdb1e50186815b3cbd73e1069a65e3a0039a3782c1ae37d3d75e2efa169
+9b1f86c69cb0badcc14bdaac62b4164764fa98bc1150104d74428ef16df7ed26979f9195c48e2ea424d88e936388ff858f52
+9dd7be20005d6e40780933100dde25866f4188fcce0867727aa3ec3d3727b1eefd9c8da62aad0ab50d599ac972d0d896fe09
+94ac06f07c477c2d2d7ca4e3d6f7423b8d99e6433b387b166ee9c810cc345f572a4093c738a18418543aa5252648b8fffe01
+b1b3327d7dfd3f6d715e2f2ca8829d3eb89b924bbe32020e4e3801dc43d592a861d80c0cfe2a15eb8535a2f198d18ae053cd
+a38d93b8a82f4928c396529787de253f1fa9655e3f8131907c0301a42019063250ec343a7d84979ee3bc0804c41117ce990a
+0ed97636fe51845edc91b77660b871c2a93bdb6e7d6fd67e385ba2a9d3f913dab8428b5002ded8887483236e33b6967fe518
+a16d2f1ae20984b7c295d7787aad88a6c3222c0ad0c5ed70725200d871354770d56c0c407f9d5ceece6190f9c6d05d3a9ec7
+ac8270b84915186229d11fa1b40a0d3c7c2d2d7cc133031bf4c9c9fcf1c6061210605232ae3a7424223947a052dab4a94e03
+feecbdffcc99655377f8ea7373ef273a203d9aa8bc6fd96c108852baa44b8953c3fb2a6dc4b357e8b32a0538e11bdc9c463e
+507f357e1bc262b60020869e56388e7bcc1b4171ffc9b6c4ebe5731107db1d0b3a8db4b1a192bfd1e7b1b13569cd8902fbe0
+c19d256cdb456bec770d4393a7a2657fc5d405e88cddbe9ab47a3631ca229fab076bbc3520e732d7986a8a39265876b6843a
+bf5b142e44ca5e9a81ae20bfae46993672cc11359f52db783e2410fb0cb7e6b13e7d6969399425d389c9336e05be6429a2f5
+17bb7a52831ef9aca0d55c471b85c2659c98ea0af02fef0ddaa3
