217b0826fcd055d19accd011a121949709b6fd56
Qihao Ye <11610812@mail.sustc.edu.cn>
2023-06-18 14:25:02 -0700

    Create Problem_0848.py

---
+++ PE/Problem_0848.py
@@ -0,0 +1,35 @@
+# Two players play a game. At the start of the game each
+# player secretly chooses an integer; the first player
+# from 1, \ldots, n and the second player from 1, \ldots, m.
+# Then they take alternate turns, starting with the first
+# player. The player, whose turn it is, displays a set
+# of numbers and the other player tells whether their
+# secret number is in the set or not. The player to correctly
+# guess a set with a single number is the winner and the
+# game ends.
+# Let p(m, n) be the winning probability of the first
+# player assuming both players play optimally. For example
+# p(1, n) = 1 and p(m, 1) = 1 / m.
+# Find \sum_{i = 0}^{20} \sum_{j = 0}^{20} p(7^{i}, 5^{j})
+# and give your answer rounded to 8 digits after the decimal
+# point.
+# ----------------------------------------------------
+# Analysis: brute force
+#           by finding the optimal strategy
+
+05f735ac73601835fc81783137ba1e1cb631edb7ee82a2843deb3afeb20db11133edfc14502a58fd3cca119911a9d1c845e0
+6b4d0511cb80e06b749602ce740ceb2ca5d849a10819556a7de3768a1d6d24e79d37019b9cf31c976ce95c0409e70e162f07
+77dfda4b9241b8523e59b86a3bb83149ae26dfa883b5766ae889327737974439e4e1d0a211d74017fe21cb3d528986aa6fee
+b15ccbc6e63351c6f516bfffd34bb471edb05871be4d832ed2fb2707a6e1a49469f48ba3953ef141465126e180898bfe603b
+fbb539aafe5a5d01e104e7f74b3b0d90a8b733a62c4d7f239f002e4439c23d311b23d209eb12d41002dbc7641bc735263438
+39a29a19786c7c2d2d7c42d1f3940e4012cf0f33f545d4ea2c30ec8ed9ef537a94f0e148adf08b6e45ef56c2cef86faa24aa
+adcd4407a5f876d6227d95ffcf62a901f786edc3924b0653e5570b7a39a615cf3095f61d1c834691266a0e90a225d3ce3801
+2a89ddcdddd2b8f263fca43d26c9fbfb06461806eab68986bbdf9c48d2277a2e55f268e9c3b80d0e4894578856f0d6cce7a6
+004fcdf10dcea1973cd6ef57553823d7260beccf5b14139c2392c3cf3566407bf6ec8a72e536ad4c2137b10b74dedf090126
+45da9ce4f4942c82fba1c83063ee5ef692f224bf75177c193a83ae8d478e3e15e51a96ed76053fe470d55dcf62eedbf9c431
+5c5894c97b8bb6d5411bb853cd059a8d7c2d2d7c580c55f2e51ebdb992a986f3c6d24e17b338c0b56f3ceaee301d8fdb19a8
+ec3bd0e05b50471786ee8a055559957574080f2b9a6af80009fd6793e955c8edeb5d225c5dea6dace4411b7a7ad50a80deec
+d7c1508ebdb9c82ec44e6809dda50d64cda1017e2b8eaaa05e597a1c0951fba9aee20e2652fa692ea3c345d4e22121c4bd6e
+fe50a09daa09b6fa880e195b03ba1358df7a69f4b75d25ba8710644b16ef7a3db3c5fb44869e56fe6c3326e2e2f8df01a7de
+29b62eab256452bef2507e4305a4e118f63b5739f9a06564932793ff1aaa006815f49c8bc01cf61583dc4d83e944275336df
+5fbcea0ea0526d101e20b0284807ea620787ba2befe5b89e2257
