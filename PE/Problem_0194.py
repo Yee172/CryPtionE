336b968de51bcecc3c420728b1273aa0575f106e
Qihao Ye <11610812@mail.sustech.edu.cn>
2023-07-26 14:19:35 -0700

    Create Problem_0194.py

---
+++ PE/Problem_0194.py
@@ -0,0 +1,51 @@
+# Consider graphs built with the units
+# A: o-----o and B: o-----o , where the units are glued
+#    | \ / |        | \ / |
+#    |  o  |        |  o  |
+#    |  |  |        |  |  |
+#    |  o  |        |  o  |
+#    |  |  |        |  |  |
+#    |  o  |        |  o  |
+#    | / \ |        | / \ |
+#    o-----o        o     o
+# along the vertical edges as in the graph
+# o-----o-----o-----o-----o
+# | \ / | \ / | \ / | \ / |
+# |  o  |  o  |  o  |  o  |
+# |  |  |  |  |  |  |  |  |
+# |  o  |  o  |  o  |  o  |
+# |  |  |  |  |  |  |  |  |
+# |  o  |  o  |  o  |  o  |
+# | / \ | / \ | / \ | / \ |
+# o-----o     o     o-----o .
+# A configuration of type (a, b, c) is a graph thus built
+# of a units A and b units B, where the graph's vertices
+# are coloured using up to c colours, so that no two adjacent
+# vertices have the same colour.
+# Let N(a, b, c) be the number of configurations of type
+# (a, b, c).
+# Find the last 8 digits of N(25, 75, 1984).
+# ----------------------------------------------------
+# Analysis: dynamic programming
+
+07b8f9f2174b28412afa687d4e4d23b6dbeff6c6b48d53109396d16b26d2e38ac33f4feec3dbef22f30ceeb7ef563611d416
+e56cee744c90b6340ad1cab405fa5b2b7e7cc3b5bdeb13d6b6863e383fb3633fded5dbca47557a26de85d8eba49aefb1632e
+05157935686fe7e28c7f19a34491cf14dab53b94146d86f304b6adc7f4d86ab757be8b9a6c59f7a395df37b170dc4b0906ec
+26df1f6489e220bdddc2d0a37434358f27deb8af8d2b2a8da2075a883eeb2a600888117a6de0a4c4956d078ba74cf3bb2073
+5c2987441021c915942d01e9a5e47da5a77ddecb9b1a559c12cbe4c11723b4f97f5aa5d266edf5f91d73c654aa28c4dedc4f
+fc3eae04f8bb7c2d2d7c9c44ccd425d26c045cd75e008ad288fd8ee71e35fb6a1b2ef5325d7aa334c4136d7c5f3f2945de79
+aa6c814b04c0ea1dadda9383425a4f651c3e003fe6f724b28403b11c105bb463a028af5e411c2bfd1673a12c1b2cca815e2f
+8c21c9c6e0278e326eed00d5563b6f58dafe0fdc4a571308063195f6e6759410bd7ccfeb7b9a615273910c1d0218ea43c3b1
+6f152b1cba5fa07c4c2ae8373b1a1ad181b5567fe0e54c8f35bb66899a8bd683846e5d548047535035deb52ff48de7f28763
+6b19fbb757047ca99e3086ac5b9d7bb947526b7cbfeba71c4ce6bd6a392f7a8740f83e8f57bdd6b9ea27fa9a01723bcd4497
+42605744c9d65102b6e16ab83dbfb3a27c2d2d7c184c44b464fa148829120e7a93dd758571468fae8a1a225bcc05714aaf8c
+8cceaa05e66ff41b0bcffeca610172d97f787688eafa7558c869bea0999ad02df21ad9a1e150526d9581b213fde90d311d94
+06b997af1f0176b0404953bf283d8431f25f52f41f032faa6993463675351f122560b3dac360ec9185a5cb543059068cd0ae
+eb20d2f286841eb5d4e5d612dfd895998ad5c1948dcdcd0a0ad61b265874b873de4f671dfe07390eb837fbbf7895cddfd1ed
+43d70cd33fc5261e701d8118c30b25355790f36fc6a17d66d399718ee1c93e5d7435237c0cf36642c70059be06abfdd10fd1
+5556b42316c9796f0d0353f0dc098c336c810dbeae903defd3ed7c2d2d7c98d53cdcf8cf5c0e02278596993679df6e91b1f5
+3c0d2b8a5e5f8c0493bb7e7f1559e136f8b8f7a84cab94cba5212368b6a39c0db7a74b3bbaba5500ccb9e78ab9ee26c487c6
+08a0e6946c18a4234e1e407d35dac69f975fd6539441149db01e7272e6226cb439b60b969631fcabf70778ddb22413101d91
+b63dadc9af223dae588ef6687069f60bb14fc9fedae971f07274c37f1c6489a56a226956dcba11c491f3918720514608e2ed
+ab86e219e6788c03315820e1a5cefa647e23b699eedffacfe79cf0458565ace515060868e1b766b34c1a9b779fd7516b9c73
+a7b24410e2c6df976af2fb3c578022b67fa17dfbe4649ab9f33f58118af9673da516bff9
