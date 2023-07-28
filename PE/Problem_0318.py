f691d9030970a50563f08c615e02a38f014d08dd
Qihao Ye <11610812@mail.sustech.edu.cn>
2023-07-28 14:50:42 -0700

    Create Problem_0318.py

---
+++ PE/Problem_0318.py
@@ -0,0 +1,40 @@
+# Consider the real number \sqrt{2} + \sqrt{3}.
+# When we calculate the even powers of \sqrt{2} + \sqrt{3}
+# we get:
+#     (\sqrt{2} + \sqrt{3})^{2} = 9.898979485566356 \cdots
+#     (\sqrt{2} + \sqrt{3})^{4} = 97.98979485566356 \cdots
+#     (\sqrt{2} + \sqrt{3})^{6} = 969.998969071069263 \cdots
+#     (\sqrt{2} + \sqrt{3})^{8} = 9601.99989585502907 \cdots
+#     (\sqrt{2} + \sqrt{3})^{10} = 95049.999989479221 \cdots
+#     (\sqrt{2} + \sqrt{3})^{12} = 940897.9999989371855 \cdots
+#     (\sqrt{2} + \sqrt{3})^{14} = 9313929.99999989263 \cdots
+#     (\sqrt{2} + \sqrt{3})^{16} = 92198401.99999998915 \cdots
+# It looks as if the number of consecutive nines at the
+# beginning of the fractional part of these powers is
+# non-decreasing.
+# In fact it can be proven that the fractional part of
+# (\sqrt{2} + \sqrt{3})^{2 n} approaches 1 for large n.
+# Consider all real numbers of the form \sqrt{p} + \sqrt{q}
+# with p and q positive integers and p < q, such that the
+# fractional part of (\sqrt{p} + \sqrt{q})^{2 n} approaches
+# 1 for large n.
+# Let C(p, q, n) be the number of consecutive nines at
+# the beginning of the fractional part of
+# (\sqrt{p} + \sqrt{q})^{2 n}.
+# Let N(p, q) be the minimal value of n such that
+# C(p, q, n) \geq 2011.
+# Find \sum N(p, q) for p + q \leq 2011.
+# ----------------------------------------------------
+# Analysis: brute force
+
+6004d05ae83a58588bd044726a29aac8659ba0669a1a9401c3c90401b555f71511c6cb94449fb55e3a55d08a073ba205af01
+a2b5504f1d19c4adbad611ca18d9f24d7a0c80fa6bbcc2c11bf0cf3564e76531609d0c378154cf22eda605288a847702160e
+266a21911fb71d18a0455c9c9743ff421db6bdd2b46829b1edad7ca82d6c62621c9498acbbf7b7d28fca5461adc994ebb79b
+e56cf1e3054e7660460c4bee08d66086914272a4ea1f51b0b9aee2246f24c0107b340eec42911eda029f23371cbd794d07fe
+ce1d34caf0ba2ca11e78ca7568524d4f7b4fa891c5bd223921c50a4e244f8c94841f2ae5312a82867739e021107419f9a5ce
+d8d7c66e8f927c2d2d7c9042b46e1eb91fa1c7751769e9f59ecce7d849ed0f13c5f6b11b3231558e52209a52d76251476e1e
+c59cdebadb0427764d93bef2e4a1caf6818f697579ca117ad217b1b57292167a41c045ebe8847e9967c7fe8767e30473fd66
+63abac11a3134e4a0460f7b31e48861f074b7fd2cea65584fc23dc39d4cb7d5251cf1c740e537aaf9e21c556ce7c3b8ef0a9
+aed2a304c5f10f868f1dfa30ce6c6128068bfbb7409fbb87f928a6227bc2c2ac3f6222bc0cb613027460a1955d0d9e158b6b
+d1187a4e89e9e56d28e62abdd922a6bcdfe0e3d7cf7faa77e25ba918e7633b527985b798f8a4acadf5e84ee588ba8e76c443
+7453ac70f12469de3c128f60a682f4a5
