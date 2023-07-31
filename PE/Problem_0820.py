507b7fcf77d275ca603a8e210e5e27c03efbb260
Qihao Ye <11610812@mail.sustech.edu.cn>
2023-07-31 12:12:30 -0700

    Create Problem_0820.py

---
+++ PE/Problem_0820.py
@@ -0,0 +1,14 @@
+# Let d_{n}(x) be the n-th decimal digit of the fractional
+# part of x, or 0 if the fractional part has fewer than
+# n digits.
+# Let S(n) = \sum_{k = 1}^{n} d_{n}(\frac{1}{k}).
+# Find S(10^{7}).
+# ----------------------------------------------------
+# Analysis: brute force
+
+8f3be853a82f6c744722f8e3a7f250aaede324109274afc5924e1777692438dcc46e133913419db80f31e56b3b6a27bfc7c2
+840aff25fcaceb83450c87737686d3413bba2af6f626181503ff7413b8841e1c3023a5cbae051fd37506dc4806a04fcb3aa2
+4e70fa05a11e2032f2fd1b8c160c4b33dc905588423cb4f17e6f98a791c58a774f8eec2679dfabbf78fdf35ebf75c7892e9c
+a545e3610322cd96f402f55180e0bbc484670f540f05aa3bc3224cc38baf7f289ac22da9a8ec2bb031092df8f1315e39022f
+8bd7a63ebd8a9a4eb69435f2cc67fef6f1ee66fd8bdfc644786e2d7f0898a5c3f871b5887f903f127ec51688394a7955d237
+07aaf4faffcf
