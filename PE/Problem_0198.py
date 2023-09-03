42c4911c770a124fffcbe75ca04a00a3ad508cb0
Qihao Ye <11610812@mail.sustech.edu.cn>
2023-09-03 13:08:02 -0700

    Create Problem_0198.py

---
+++ PE/Problem_0198.py
@@ -0,0 +1,38 @@
+# A best approximation to a real number x for the denominator
+# bound d is a rational number \frac{r}{s} (in reduced form)
+# with s \leq d, so that any rational number \frac{p}{q}
+# which is closer to x than \frac{r}{s} has q > d.
+# Usually the best approximation to a real number is uniquely
+# determined for all denominator bounds. However, there
+# are some exceptions, e.g. \frac{9}{40} has the two best
+# approximations \frac{1}{4} and \frac{1}{5} for the
+# denominator bound 6. We shall call a real number x ambiguous,
+# if there is at least one denominator bound for which
+# x possesses two best approximations. Clearly, an ambiguous
+# number is necessarily rational.
+# How many ambiguous numbers x = \frac{p}{q}, 0 < x < \frac{1}{100},
+# are there whose denominator q does not exceed 10^{8}?
+# ----------------------------------------------------
+# Analysis: Stern–Brocot tree
+
+8c434daa4094c0128f282271fd5a337cfb8c001e1513f1ff2e837caa3c6c5260501a8033a0bf7dc6057772657d0db42082bd
+bc8548c0744b978155200acd37135c12f803eedfa26b9392bd9df0a4f2dcace0a997e63c6fb6935e0f12bec92fea10b56fe9
+ed657411ebe4318f640c9cef332789307c37e73c3fc5b947f6e4fc44e01bbff17f5a81c7f10ab7f4000ceaf70dfadef246fa
+0f41ecf6a5f7abc6f47aaa887406a42b6acee209c93617c739f3eb24631bd914e13b1a23a0b18db6a16a6b697ae24c7ab0a9
+3ea3c23236ef967884e76223f3f2efb1925d9cde064de95587b98af516e5b4c66a942d4d3ee06a24d549a6ff67a09b641176
+244e1620473b7c2d2d7c50b789838b0ae8852a6ef57c0ae095178cfcd4c6023ad4d0c2b779d8bdfdba50ab4b12ebb63fd4c2
+8ad92b0944f891f043b81c29094d1930b3c7790ffd7845296e988dbd3b766f4a90a202b9d7e24e90fdf5c4c5de218c459fa9
+d75451c76591dfec32788415554d2233d961e1e0d484ab31a3f9aa411c1397dcf170003c07d9c8f90a138d9223d50515e2b6
+ad0bed75fca654a3c3ef827aed44c48ff90c9fcadb5572d26c0661d95b0a7e2fcf181242c22864b0d143331ae617e8f76c45
+f9afb87359a1516c43c0d62f8ccd80d58e6e098ccfb7fc3a178908c939c15e6adb90f693e4067796856350b2999691517402
+1dedc94ae34c26168c167641efdbf1287c2d2d7c333a9cb1c4b31509d807d38631424f1c218a3334811f316441b71eed8979
+9375d0777711a0cce2fd03f3d9bf0a7522c91cd2cf73bef058986405f954bd766ba6a87df83207b3aa834a61d7a23cd322d2
+6b9bb4bfb2ceae1c487be0f148e1fbb256f7f8b441eaa07e42632a5c71a456d21b5acba34948f9a627492b341f9f36033f5a
+c278a122477ffdf2b1267e86400d5b4d232ce68b24ca9efa4e0518344fafb2fc58dc0434e51b1b236f670f45d0b6c8c6e778
+cb6907a4a6b58d72a7dd391a6bb91863bd4c98a1f86285f22fd18abc7688231add72bdf9bab295e0b97c4a33f7553f712ac0
+433f6c7af9d9ce8c5cd034efeb5d1efbddc71e13dc3f41f83c7e7c2d2d7c4fe7fbbda8f98f3f5003dfd5ebbe34130bf88f94
+fd79fc75e2010ea9a27383e87eb8f8c310a5885ab6e05c45245675af18e21430e717a776a341de2994f1d3d35718badc10a6
+fdec86e7d833e39c0aed9a00affe201e2919a368c36fea38eb001dcbb4afb0febb6188c120a2cc8320fe2e9656fef6bd2751
+0478d691921017a5106ef79b924cb30684c34bcf05db381e45dbf4ba70542f5e6b21bca2339e8a76105b16830bdb261a0b11
+b7576867ff51dbda753bd6372c997a71ed16341a2e5868fe9a67d77ad535a5eb53465fd0b909e650ebc57499a9d92bfe870a
+343bc2dfbd2ff32d5c3b16f2aab166c74f4ae20d6ed440c2c1eb9566a6f5bc8888b7c22f
