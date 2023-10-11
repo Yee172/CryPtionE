f3704b2656c5796ac7d76066724640467aafe400
Qihao Ye <11610812@mail.sustech.edu.cn>
2023-10-10 22:02:25 -0700

    Create Problem_0855.py

---
+++ PE/Problem_0855.py
@@ -0,0 +1,31 @@
+# Given two positive integers a, b, Alex and Bianca play
+# a game in (a b) rounds. They begin with a square piece
+# of paper of side length 1.
+# In each round Alex divides the current rectangular piece
+# of paper into a \times b pieces using a - 1 horizontal
+# cuts and b - 1 vertical ones. The cuts do not need to be
+# evenly spaced. Moreover, a piece can have zero width / height
+# when a cut coincides with another cut or the edge of
+# the paper. The pieces are then numbered 1, 2, ..., a b
+# starting from the left top corner, moving from left to
+# right and starting from the left of the next row when
+# a row is finished.
+# Then Bianca chooses one of the pieces for the game to
+# continue on. However, Bianca must not choose a piece
+# with a number she has already chosen during the game.
+# Bianca wants to minimize the area of the final piece
+# of paper while Alex wants to maximize it. Let S(a, b)
+# be the area of the final piece assuming optimal play.
+# Find S(5, 8). Give your answer in scientific notation
+# rounded to ten significant digits after the decimal
+# point. Use a lowercase e to separate the mantissa and
+# the exponent.
+# ----------------------------------------------------
+# Analysis: brute force
+
+393bc6b8b367f4399f503643006c5d9d5b8ac51ca5d3303844ee01804636837106d5d10865bb037e2648d6d4a9f3bb133888
+6990962b2770e134556c9d7cad3f70e4e8c864fae7cd427c30098e472c541b75711974f763a9b223915232d7ab218c642679
+abdf2d338ed095ff342de57f684e889c78cdf7aa856779d13d32a8aa4619deadd9b75ea52254679c60756613f8d07fd84dc1
+011660a686838dd509df519868fe85338f6d8e78d9eb7e5a5b91cf11809046cfcc229804e1acef62935b7df45b3671c1570c
+08b23c374367bd3bbdea5171d5ebe98a698f1e925f334e904d8f2623f0687de66471f7eaf7a671171b772902b883e273483b
+f04bb4f308d3
