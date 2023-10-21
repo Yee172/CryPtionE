92df2a152947821ca8a650cf8577dde5f070623e
Qihao Ye <11610812@mail.sustech.edu.cn>
2023-10-20 19:05:15 -0700

    Create Problem_0539.py

---
+++ PE/Problem_0539.py
@@ -0,0 +1,31 @@
+# Start from an ordered list of all integers from 1 to n.
+# Going from left to right, remove the first number and
+# every other number afterward until the end of the list.
+# Repeat the procedure from right to left, removing the
+# right most number and every other number from the numbers
+# left. Continue removing every other numbers, alternating
+# left to right and right to left, until a single number
+# remains.
+# Let P(n) be the last number left starting with a list
+# of length n.
+# Let S(n) = \sum_{k = 1}^{n} P(k).
+# Find S(10^{18}) mod 987654321.
+# ----------------------------------------------------
+# Analysis: brute force
+
+449df6326cc2c46eb47345f47b5c24e3b2e098d36979e9ed011e6df86c16fb995029112fea9a663bcb77b9bf9431f6b9895d
+5d01a004d3fd2d20ae077dfbb059e3f27d1ada6b9078657e49045b58c62df08144ccb9fcd71f6987fdc1b97e584023f85d47
+84ae56d4e7c3448fdb9fa10a498423174b6e57e6d1ffa02aa19948be18fc8d4aaf730c9a69bce79b7d07347b3d468725160a
+1740596a22b9d3e77dd379b4086922773592ca081ebfbdc134c2b674a479c876d7008f0a00c20e3382804fc344470adce447
+015b4acc04580353f64bdb2862fc69539f2c6423af611f3e1c1493dfda25dbd89065414f8555f39677dfd5d7da8ab64ee8c5
+f41858468fd97c2d2d7c887900689e253a970880a68c7df44b9f3330f388e2d05ff95982f1737adcc19c29ad0204c4054f42
+b2e9ee723a59fa20361eea2285ae5fedb38bbb5a49a2ac87de63591eaa15ef72bcc79d71e29c3ab4fcb62d82669d1ba7656a
+3185493bb37080b8ef786e03074debec098a927a3e41625d9614e53879b8df0623784ebe2d93b525057769b3c0e7c14a51b9
+d6dbd4435d6092fbb45569f00ca7ead1b6bffcbe7a9ff5ce31d86c9b02cffdfd42be788dd39f7e809d1dd233ee216c03a85b
+b9a15f54716064f10f0f0ba402175e25fe181aef35abf88eb3b3acc7bd8cb472b88dfd1459704d2991cf8bae1b2a68781255
+ca151f85e1f5a8ae7c15f69b139e252c7c2d2d7c4f770a7f94c22e0fb141c7de19444d6b6167c6c8c4a87cda8440e0c27693
+dffa31deb556ab6d2ba1f5a75ea874321c512c5f17588197991fb4ce2d60eae51e4d29b2cda7dde63379fc6a526a94cebc3c
+135e0a1e8e7b829d02475aa86fa8c17f21cfff0f5487bf8d52720a18cce8860eddfb25722d43a4735cd31042dc9b22c04a65
+075893c1086a0c12b0452113975cd505d4d714255d09e8f16ccda7b3b4c05d82258135c2e41861b2c0b0ed4d3e0b0141b0a8
+879b4d45254dcc87ab7406fb84380d0a7faab4ff0a799fa19d635ca0aa118dcd703c7b908fe009bb68bd8b504dd848adbc25
+d64af31f77c7e651cf75f1a4d236359f471900ed8f52d52e07d4
