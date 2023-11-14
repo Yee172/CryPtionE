bdb66daceafd112494273dbf26e2e0d8b2af1a56
Qihao Ye <11610812@mail.sustech.edu.cn>
2023-11-13 21:22:35 -0800

    Create Problem_0297.py

---
+++ PE/Problem_0297.py
@@ -0,0 +1,26 @@
+# Each new term in the Fibonacci sequence is generated
+# by adding the previous two terms.
+# Starting with 1 and 2, the first 10 terms will be:
+# 1, 2, 3, 5, 8, 13, 21, 34, 55, 89.
+# Every positive integer can be uniquely written as a
+# sum of nonconsecutive terms of the Fibonacci sequence.
+# For example, 100 = 3 + 8 + 89.
+# Such a sum is called the Zeckendorf representation
+# of the number.
+# For any integer n > 0, let z(n) be the number of terms
+# in the Zeckendorf representation of n.
+# Find \sum z(n) for 0 < n < 10^{17}.
+# ----------------------------------------------------
+# Analysis: brute force
+
+91120c3948f4475400ec96caa724c0a923cdba327e675f4fbd30f22513db21d5cf43336095faef4c27c9480869bf2dbf1b0e
+a2b71f7198afb3cc45747ae47552c86195178b40d1f5635fb4bc2294c4d798e3e0a897cde510cf18090d1c821b2a29447320
+b7ce1adf053e17e2e1e8d6b93e905117b807d4a01260e5b8065a9e3025cf96ca8f6c6d5878d9182cff83450fd011eeaf32f3
+6c3ec32cbd6b58005dbb8472b3cae7e4d4f131ca48fe6ba02ff0a1ad237aebf93fa3f84292124d5f06adb4c57546a2c127b3
+aa90804baeb0f0e18323d184f92bf76e070ded7089effed3d21f7e147c09a5c34ee54dae78c295b8b0ba38a0007925b9024a
+89f05b71de577c2d2d7c259359b7b3c03d618fb6b58ebf87ae00802081189fb30973e517a49506cf77685777ebf0b06f2cb2
+60363d6fd4f92630a299eb200b86d235106ab9f00bf0054574cdced5d174e3ade5ad3a7f8c2f3d6652723ae7c6fac65adf4b
+a19ff0bbe7a2592b8f8c108df4c2772388fdac7b3e661efaf0f01719f7f4b78b5e8b8d77b87aabcb806902fe547b43f91b11
+daff253643ea39caee4c7a70cc0e1431535cf01b6f73ab7e6a2a1e7e73f3e90e36c67f146e6f69930bcc1c9d0b931426aa09
+0d321a047736ff57ff391550ec25a5ea252d692d231bccf9a8b1bab696ae1db34e770705bd5b20cfc2a542dc8e9c58768555
+0cdd206fb872e72bd58dcea313f538c2
