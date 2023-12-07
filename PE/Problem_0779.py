aead64c8c0827f720b16aad75c56a0348d4706f9
Qihao Ye <11610812@mail.sustech.edu.cn>
2023-12-06 23:40:18 -0800

    Create Problem_0779.py

---
+++ PE/Problem_0779.py
@@ -0,0 +1,24 @@
+# For a positive integer n > 1, let p(n) be the smallest
+# prime dividing n, and let \alpha(n) be its p-adic order,
+# i.e. the largest integer such that p(n)^{\alpha(n)}
+# divides n.
+# For a positive integer K, define the function f_{K}(n) by:
+#     f_{K}(n) = \frac{\alpha(n) - 1}{(p(n))^{K}}.
+# Also define \overline{f_{K}} by:
+#     \overline{f_{K}} = \lim_{N \to \infty} \frac{1}{N} \sum_{n = 2}^{N} f_{K}(n).
+# Find \sum_{K = 1}^{\infty} \overline{f_{K}}. Give your
+# answer rounded to 12 digits after the decimal point.
+# ----------------------------------------------------
+# Analysis: brute force
+
+467d606bf60554df96485e4602ded3449668899eb04384a7ea730b1efab8a6349492391bbd28bb39e15413bab236960b5d49
+08a31f431f73c4946bfbfc28b53b76a0299d053dc6cb9c082af83ca669f5f474924219839218d3caaf9c76d771843489f478
+89221978deb34661f56aa8962b91fb78904c44b324c995b206e5f628171bc132d88a926f61d58cdd9363f5075c9c98066c08
+9c5a42720aba8759b0dc8b8826c1830792493026759042ee11b9e4e1c1d6bf7ab4897673d7f602a24a84dc1269fe94e04df6
+ce054386c9208a9eed0b6447ecefacd9367a852e27764f9c66eec0f88bf9576a5a6fef74889a9a687f0270c12a3b77b0e5a4
+1bcc40c3241f7c2d2d7cac70e1d1fef131b32f3ec86ecd046da3499bc0e0d9f6d3d50aee8a68f5a7ae2cfda6b3f833bdcd82
+82d0e354a43381eb8d2f6d4bb948b75106cd5cda14e166e1876395df2d98461191722cc7253f2adb89d748c183eb231f65c7
+8ef9d790af472b949dec2b415bb4ed32655a803891ee151f004b3c67cab889c6682cc0cad31cbadab252657136ed7d71bdc3
+eef649690c15ba7a931748ca478bb3773bd2c098a8d0cb0bb351deec1daf97572c40594ddcc18a1af3d5ce214094d0069648
+e91b6e76e93b75fc992a1fcd7fe3625cf92b2e20416f199eded765b9e5491ae2f7bb94e67711ecfacaef035ea8f435bd3e35
+3dcb199cb78d4aa8914392475ef83dc0
