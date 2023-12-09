b9b3273bb9859d837551bce15ce44a757875da2b
Qihao Ye <11610812@mail.sustech.edu.cn>
2023-12-09 07:51:54 -0800

    Create Problem_0866.py

---
+++ PE/Problem_0866.py
@@ -0,0 +1,39 @@
+# A small child has a "number caterpillar" consisting of
+# N jigsaw pieces, each with one number on it, which,
+# when connected together in a line, reveal the numbers
+# 1 to N in order.
+# Every night, the child's father has to pick up the pieces
+# of the caterpillar that have been scattered across the
+# play room. He picks up the pieces at random and places
+# them in the correct order.
+# As the caterpillar is built up in this way, it forms
+# distinct segments that gradually merge together.
+# Any time the father places a new piece in its correct
+# position, a segment of length k is formed and he writes
+# down the k-th hexagonal number k (2 k - 1). Once all
+# pieces have been placed and the full caterpillar constructed
+# he calculates the product of all the numbers written
+# down. Interestingly, the expected value of this product
+# is always an integer.
+# Find the expected value of the product for a caterpillar
+# of N = 100 pieces.
+# Give your answer modulo 987654319.
+# ----------------------------------------------------
+# Analysis: brute force
+
+3c00dec658329d94e1dd9f769e839dc308e3a3a360f8c2fa54adfc5bede2c9e9caeaa2b2d449bfe55a3d59dca1184506e035
+98de477c08bd83397263bb84eab89e8730cae7763deae2a6d350ae34f466a7185435cba951acb93695856dc3055355e5f67f
+36e9638e8a970334a8a65fd720ecbb2388c33b67fb6878e6234c960cb13854cd535563b5421d0c3ade3b2ed32567ee694883
+f028b8269f9c245f364b557877de67c49b03b5747b7defba72cf854b0c581cf0d4ad74b408617e356b1a741c88e2b7d5052f
+62ed254c1aa7c0a33848f6fdc45111fe7f2acf229185ac7920ddd23fd01da582e0fbc8716b5efdf982af9d3f3192a62d3f57
+2c526ba387c17c2d2d7c7a648899e1455cda2b905cb5827a6a0c626fe9ea30d512985220591a1d5465eee32257a4dbcc33e5
+92c4b547a0b0b70674aaf284987bdeef78c8c1197da322415e9d4408053e661f6483215ae8e7f07a12e1976aa7d638697bf2
+61f8af2ee429c914e2a1efc238700c56b039e4fd87be71cca1a0c0dbcde6b702754c7870adb7e6be49a05401e57f63b08c6b
+6b6b9219845d29d8137706240a50704e67829ec20f0d745e44f9814f77852a586d8e44f04d41bb96b6283af04161e8a73426
+5f76a7aeeb970e632d966d6b6dd4c48b9cbd13e7fd662c8933ebb3610fdff0f10fa15af097170e233266baa988a43c85e255
+919bd9e94bdce2b12db45c15df9663657c2d2d7c3340b1b2eac2bdd1a6cfe8066871e7bbc8a641e63fd9604a240618f9f59a
+38e8c524d1e8c74e82fa8cb708a2731bc3c5a2ab897cccd59bd950f85b38da1a81a38e6193fb5a98669366c3cb8fe5ee06bd
+59cd258baa9c807db65355a9b279af759bd70fe04c35d85c797f6deb62809a162557ce8ed4cc58bd943da11dc7c7d01b03c0
+19d841d0384fae538e552d78817159b476dfdb4bebf440b89e21ff008fa23e7ea61ffdc518c11ca68fa9fd6babeb04f34073
+f666031a0ccafdb318a23c960078fe4d9497008694941e44ebb16ac0db820d2b189b4cf6d1a2bcfc418314960bae0e61bd72
+31780eb1cbaf386e85a955db1328e530c444b8ee1fab180aa8f6
